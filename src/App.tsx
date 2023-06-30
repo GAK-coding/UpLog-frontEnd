621f7d02ab2629dc86a751af4e63f6afc74aeaf2
5hyun <qhslsl@gmail.com>
2023-07-01 07:17:10 +0900

    Feat: esLint, prettier 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,27 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
-import tw, { styled, css, theme } from 'twin.macro'
+import tw, { styled, css, theme } from 'twin.macro';
 
 const Input = styled.div`
   ${css`
     -webkit-tap-highlight-color: transparent; /* add css styles */
     background-color: ${theme`colors.red.500`}; /* add values from your tailwind config */
     ${tw`text-green-400 border-2`}; /* tailwind classes */
-    
+
     &::selection {
       ${tw`text-purple-500`}; /* style custom css selectors with tailwind classes */
     }
   `}
-`
+`;
 
 function App() {
-
-    return (
-        <div>
-            <h1 className="text-2xl font-bold underline">
-                Hello world!
-
-            </h1>
-            <Input>하이</Input>
-        </div>
-    )
+  return (
+    <div>
+      <h1 className="text-2xl font-bold underline">Hello world!</h1>
+      <Input>하이</Input>
+    </div>
+  );
 }
 
-export default App
+export default App;
