10f7eafd565af126253af7501b4b6924ccdec301
5hyun <qhslsl@gmail.com>
2023-07-01 08:45:21 +0900

    Feat: react dev tool 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,29 @@
 import './App.css';
 import { RecoilRoot } from 'recoil';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { useState } from 'react';
 
-// import tw, { styled, css, theme } from 'twin.macro';
-//
-// const Input = styled.div`
-//   ${css`
-//     -webkit-tap-highlight-color: transparent; /* add css styles */
-//     background-color: ${theme`colors.red.500`}; /* add values from your tailwind config */
-//     ${tw`text-green-400 border-2`}; /* tailwind classes */
-//
-//     &::selection {
-//       ${tw`text-purple-500`}; /* style custom css selectors with tailwind classes */
-//     }
-//   `}
-// `;
+const queryClient = new QueryClient();
 
 function App() {
+  const [showDevtools, setShowDevtools] = useState(true);
+
   return (
-    <RecoilRoot>
-      <BrowserRouter>
-        <div>
-          <h1 className="text-2xl font-bold underline">Hello world!</h1>
-        </div>
-        <Routes>
-          <Route></Route>
-        </Routes>
-      </BrowserRouter>
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <BrowserRouter>
+          <div>
+            <h1 className="text-2xl font-bold underline">Hello world!</h1>
+          </div>
+          <Routes>
+            <Route></Route>
+          </Routes>
+        </BrowserRouter>
+      </RecoilRoot>
+      {showDevtools && <ReactQueryDevtools />}
+    </QueryClientProvider>
   );
 }
 
