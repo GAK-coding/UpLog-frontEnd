1cabb9c0bc4a27aaeb180f76dc51cb31b813bf92
5hyun <qhslsl@gmail.com>
2023-07-01 08:36:38 +0900

    Feat: App.tsx에 recoil, router 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import { useState } from 'react';
 import './App.css';
+import { RecoilRoot } from 'recoil';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import tw, { styled, css, theme } from 'twin.macro';
-
-const Input = styled.div`
-  ${css`
-    -webkit-tap-highlight-color: transparent; /* add css styles */
-    background-color: ${theme`colors.red.500`}; /* add values from your tailwind config */
-    ${tw`text-green-400 border-2`}; /* tailwind classes */
-
-    &::selection {
-      ${tw`text-purple-500`}; /* style custom css selectors with tailwind classes */
-    }
-  `}
-`;
+// import tw, { styled, css, theme } from 'twin.macro';
+//
+// const Input = styled.div`
+//   ${css`
+//     -webkit-tap-highlight-color: transparent; /* add css styles */
+//     background-color: ${theme`colors.red.500`}; /* add values from your tailwind config */
+//     ${tw`text-green-400 border-2`}; /* tailwind classes */
+//
+//     &::selection {
+//       ${tw`text-purple-500`}; /* style custom css selectors with tailwind classes */
+//     }
+//   `}
+// `;
 
 function App() {
   return (
-    <div>
-      <h1 className="text-2xl font-bold underline">Hello world!</h1>
-      <Input>하이</Input>
-    </div>
+    <RecoilRoot>
+      <BrowserRouter>
+        <div>
+          <h1 className="text-2xl font-bold underline">Hello world!</h1>
+        </div>
+        <Routes>
+          <Route></Route>
+        </Routes>
+      </BrowserRouter>
+    </RecoilRoot>
   );
 }
 
