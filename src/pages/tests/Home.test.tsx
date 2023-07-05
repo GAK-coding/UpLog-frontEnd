7ed65489eb4912f422f57d3eb26f1f06d7d8125a
5hyun <qhslsl@gmail.com>
2023-07-05 09:01:15 +0900

    Feat; jest 설정 완료

--- src/pages/tests/Home.test.tsx
+++ src/pages/tests/Home.test.tsx
@@ -1,11 +1,15 @@
 import { describe, expect, test } from '@jest/globals';
 import Home from '../Home';
-import {render} from "@testing-library/react";
+import {render, screen} from '@testing-library/react'
+import '@testing-library/jest-dom'
+
 
 describe('Home', () => {
   test('resolves to lemon', () => {
     // make sure to add a return statement
     // return expect(Promise.resolve('lemon')).resolves.toBe('lemon');
-
+    // expect(2 + 2).toBe(4);
+    render(<Home />)
+    expect(screen.getByText('여긴 홈')).toBeInTheDocument();
   });
 });
