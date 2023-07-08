a0546a2b934dd96721b52860b32af5ff17312f6e
5hyun <qhslsl@gmail.com>
2023-07-08 18:55:28 +0900

    Feat: 일단 이미지화는 성공

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 // vite.config.ts
-import { defineConfig } from 'vitest/config'
+// import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
+
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
