9ffaea4c754418f38bc4454747b6abe0edbcc9e7
5hyun <qhslsl@gmail.com>
2023-07-11 01:35:43 +0900

    Feat: 헤더 값 고정 및 레이아웃 수정

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 
 export default function SignUp() {
   return (
-    <main className={'h-88vh flex justify-center items-center'}>
-      <section className={'mt-12vh border-solid border-2 border-header-gray w-39rem h-42rem'}>
-        <div>
-          <img className={'h-16'} src={'/images/login.png'} alt={'login logo'} />
-        </div>
-        <div></div>
-        <div></div>
-      </section>
-    </main>
+    <>
+      <main className={'h-full flex justify-center items-center'}>
+        <section className={'mt-[-7rem] border-solid border-2 border-header-gray w-39rem h-42rem'}>
+          <div>
+            <img className={'h-16'} src={'/images/login.png'} alt={'login logo'} />
+          </div>
+          <div></div>
+          <div></div>
+        </section>
+      </main>
+    </>
   );
 }
