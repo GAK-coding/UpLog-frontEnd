45cf99e34e5321e974510981732bd7a79561311f
5hyun <qhslsl@gmail.com>
2023-07-10 20:28:56 +0900

    Chore: 회원가입 페이지 만드는중

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 export default function SignUp() {
   return (
-    <section className={'border-solid border-2 border-header-gray'}>
-      <img src={'/images/login.png'} alt={'login logo'} />
-    </section>
+    <main className={'h-88vh flex justify-center items-center'}>
+      <section className={'mt-12vh border-solid border-2 border-header-gray w-39rem h-42rem'}>
+        <div>
+          <img className={'h-16'} src={'/images/login.png'} alt={'login logo'} />
+        </div>
+        <div></div>
+        <div></div>
+      </section>
+    </main>
   );
 }
