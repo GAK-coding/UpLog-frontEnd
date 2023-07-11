c5ce32f398fd747efebec97b482556c620138025
5hyun <qhslsl@gmail.com>
2023-07-12 01:04:54 +0900

    Feat: useInput 커스텀 훅

---
+++ src/hooks/useInput.ts
@@ -0,0 +1,18 @@
+import { Dispatch, SetStateAction, useCallback, useState, ChangeEvent } from 'react';
+
+type ReturnTypes<T = ChangeEvent<HTMLInputElement>> = [
+  T,
+  (e: ChangeEvent<HTMLInputElement>) => void,
+  Dispatch<SetStateAction<T>>
+];
+
+/** useInput은 useState에서 onChange 함수까지 간편하게 적용할 수 있는 커스텀 훅이다. **/
+const useInput = <T = ChangeEvent<HTMLInputElement>>(initialValue: T): ReturnTypes<T> => {
+  const [value, setValue] = useState(initialValue);
+  const handler = useCallback((e) => {
+    setValue(e.target.value as unknown as T);
+  }, []);
+  return [value, handler, setValue];
+};
+
+export default useInput;
