90437aff34ea5694ca7e4ac11a35bd35ae33b056
5hyun <qhslsl@gmail.com>
2023-07-12 11:34:00 +0900

    Chore: event 타입 지정

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -9,7 +9,7 @@ type ReturnTypes<T = ChangeEvent<HTMLInputElement>> = [
 /** useInput은 useState에서 onChange 함수까지 간편하게 적용할 수 있는 커스텀 훅이다. **/
 const useInput = <T = ChangeEvent<HTMLInputElement>>(initialValue: T): ReturnTypes<T> => {
   const [value, setValue] = useState(initialValue);
-  const handler = useCallback((e) => {
+  const handler = useCallback((e: any) => {
     setValue(e.target.value as unknown as T);
   }, []);
   return [value, handler, setValue];
