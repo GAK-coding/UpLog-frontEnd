00231dac39ce017d8234265c0ae87b5270ae82c7
CHCHAENG <oco6029@naver.com>
2023-07-14 00:57:50 +0900

    Style : 검색창 + 아이콘 배치 완료

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-import { BsSunFill } from 'react-icons/bs';
+import { BsBellFill, BsQuestionCircle, BsSearch, BsSunFill } from 'react-icons/bs';
+import { FaUserCircle } from 'react-icons/fa';
 
 export default function Header() {
   // 로그인 하기 전, border-bottom을 보여주지 않기 위한 로직
@@ -9,7 +10,8 @@ export default function Header() {
 
   const navigate = useNavigate();
 
-  const userprofile = '/images/test_userprofile.png';
+  // TODO: 실제 userprofile 값으로 변경하기
+  const userprofile = '';
 
   useEffect(() => {
     setIsLogin(pathname === '/login' || pathname === '/signup');
@@ -22,23 +24,25 @@ export default function Header() {
       }`}
     >
       {/*로고 + 글자 (메인페이지로 이동)*/}
-      <nav className={'flex flex-row cursor-pointer ml-32'} onClick={() => navigate('/')}>
-        <img className={'mr-4 h-12'} src={'/images/mainLogo.png'} alt={'main-logo'} />
-        <span
-          className={
-            'flex justify-center items-center font-logo text-[30px] font-semibold text-gray-dark'
-          }
-        >
-          upLog
-        </span>
-        {/*TODO : 스토리지 값 체크후에 변경하기*/}
+      <div className={'flex ml-32'}>
+        <nav className={'flex flex-row cursor-pointer'} onClick={() => navigate('/')}>
+          <img className={'mr-4 h-12'} src={'/images/mainLogo.png'} alt={'main-logo'} />
+          <span
+            className={
+              'flex justify-items-center items-center font-logo text-[2.5rem] font-semibold text-gray-dark'
+            }
+          >
+            upLog
+          </span>
+        </nav>
 
+        {/*TODO : 스토리지 값 체크후에 변경하기 (조건으로 렌더링 여부 바꿔야함)*/}
         <div
           className={
-            'flex justify-center items-center ml-4 h-11 border-solid border-r border-header-gray'
+            'flex flex-row justify-center items-center ml-4 h-11 border-solid border-r border-header-gray'
           }
         ></div>
-      </nav>
+      </div>
 
       {/*로그인하지 않은상태*/}
       {!isLogin && (
@@ -58,22 +62,22 @@ export default function Header() {
         //     &nbsp; 회원가입
         //   </span>
         // </div>
-        <div className={'flex mr-12 font-bold items-center border-solid border black'}>
-          <img src={userprofile} alt="userprofile" className={'h-11'} />
-          {/*<BsSunFill className={'text-3xl mr-4 fill-gray-dark cursor-pointer'} />*/}
-          {/*<span*/}
-          {/*  className={'font-bold text-gray-dark text-lg cursor-pointer'}*/}
-          {/*  onClick={() => navigate('/login')}*/}
-          {/*>*/}
-          {/*  로그인&nbsp;*/}
-          {/*</span>*/}
-          {/*<span className={'text-bold text-gray-dark text-lg'}>•</span>*/}
-          {/*<span*/}
-          {/*  className={'text-gray-dark text-lg cursor-pointer'}*/}
-          {/*  onClick={() => navigate('/signup')}*/}
-          {/*>*/}
-          {/*  &nbsp; 회원가입*/}
-          {/*</span>*/}
+        <div
+          className={
+            'flex w-[31rem] h-full justify-between mr-12 font-bold items-center border-solid border black'
+          }
+        >
+          <div className={'w-48 mt-1.5 h-3/5 border-solid border border-gray-light rounded-lg'}>
+            <BsSearch className={'text-2xl fill-gray-dark '} />
+          </div>
+          <BsSunFill className={'text-[2.5rem] fill-gray-dark cursor-pointer'} />
+          <BsBellFill className={'text-[2.5rem] fill-gray-dark cursor-pointer'} />
+          <BsQuestionCircle className={'text-[2.5rem] fill-gray-dark cursor-pointer'} />
+          {userprofile === '' ? (
+            <FaUserCircle className={'text-[2.5rem] fill-gray-dark cursor-pointer'} />
+          ) : (
+            <img src={userprofile} alt="userprofile" className={'h-[2.6rem] cursor-pointer'} />
+          )}
         </div>
       )}
 
