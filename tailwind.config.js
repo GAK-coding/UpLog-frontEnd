71d6b3be32c17f0c4ff510f73c975211abb04500
CHCHAENG <oco6029@naver.com>
2023-07-13 13:58:20 +0900

    Style : tailwind.config.js 정리

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,30 +14,11 @@ export default {
         logo: "'Baloo Tammudu 2', cursive",
       },
       height: {
-        '42rem': '42rem',
         noneHeader: 'calc(100vh - 5.5rem)',
-        'header-height': '5.5rem',
-        '47.5%': '47.5%',
-        '18%': '18%',
-        '82%': '82%',
-        '45%': '45%',
-      },
-      minHeight: {
-        '42rem': '42rem',
-      },
-      width: {
-        '39rem': '39rem',
-      },
-      margin: {
-        '[-5.5rem]': '-5.5rem',
-        '[-1rem]': '-1rem',
       },
       boxShadow: {
         'sign-up': '2px 2px 10px 1px rgba(0, 0, 0, 0.15), -2px -2px 10px 0px rgba(0, 0, 0, 0.15)',
         'sign-up-info': '-2px 2px 8px 0px rgba(0, 0, 0, 0.15), 2px 2px 8px 1px rgba(0, 0, 0, 0.15)',
-      },
-      border: {
-        '0.6px': '0.6px',
       },
     },
   },
