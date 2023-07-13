ea13cc03f484d4af4c2e28e2e124adb7e9969fe8
5hyun <qhslsl@gmail.com>
2023-07-13 19:47:18 +0900

    Feat: 타이머 분, 초 구하는 함수

---
+++ src/utils/convertMinutes.ts
@@ -0,0 +1,11 @@
+export function convertMinutes(seconds: number): string {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+  const formattedSeconds = remainingSeconds < 10 ? `0${remainingSeconds}` : `${remainingSeconds}`;
+
+  if (minutes === 0) {
+    return `00:${formattedSeconds}`;
+  }
+
+  return `${minutes}:${formattedSeconds}`;
+}
