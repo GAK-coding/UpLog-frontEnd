5c7acb53ba35068cddbf398863b896c63b1bca30
CHCHAENG <oco6029@naver.com>
2023-07-13 15:37:22 +0900

    chore : 주석 추가

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { BsSunFill } from 'react-icons/bs';
 
 export default function Header() {
-  // 로그인, 회원가입이면 border-bottom을 보여주지 않기 위한 로직
+  // 로그인 하기 전, border-bottom을 보여주지 않기 위한 로직
   const { pathname } = useLocation();
   const [isLogin, setIsLogin] = useState(pathname === '/login' || pathname === '/signup');
 
@@ -15,30 +15,39 @@ export default function Header() {
 
   return (
     <header
-      className={`flex flex-row justify-between items-center ml-32 mr-12 pt-2 h-full border-solid border-red-400 ${
-        isLogin ? '' : 'border-solid border border-header-gray'
+      className={`flex flex-row justify-between items-center pt-2 h-full ${
+        isLogin ? '' : 'border-solid border-b border-header-gray'
       }`}
     >
-      <nav
-        className={'flex flex-row border-solid border-red-400 cursor-pointer'}
-        onClick={() => navigate('/')}
-      >
-        <img
-          className={'mr-4 h-12 border-solid border black'}
-          src={'/images/mainLogo.png'}
-          alt={'main-logo'}
-        />
-        <span
-          className={
-            'flex justify-center items-center font-logo text-4xl font-bold border-solid border black'
-          }
-        >
+      {/*로고 + 글자 (메인페이지로 이동)*/}
+      <nav className={'flex flex-row cursor-pointer ml-32'} onClick={() => navigate('/')}>
+        <img className={'mr-4 h-12'} src={'/images/mainLogo.png'} alt={'main-logo'} />
+        <span className={'flex justify-center items-center font-logo text-4xl font-bold'}>
           upLog
         </span>
       </nav>
 
+      {/*로그인하지 않은상태*/}
       {!isLogin && (
-        <div className={'flex border-solid border black font-bold items-center cursor-pointer'}>
+        <div
+          className={'flex mr-12 font-bold items-center cursor-pointer border-solid border black'}
+        >
+          <BsSunFill className={'text-3xl mr-4 fill-dark-gray'} />
+          <span className={'text-bold text-dark-gray text-lg'} onClick={() => navigate('/login')}>
+            로그인 •
+          </span>
+
+          <span className={'text-dark-gray text-lg'} onClick={() => navigate('/signup')}>
+            &nbsp; 회원가입
+          </span>
+        </div>
+      )}
+
+      {/*로그인 후*/}
+      {isLogin && (
+        <div
+          className={'flex mr-12 font-bold items-center cursor-pointer border-solid border black'}
+        >
           <BsSunFill className={'text-3xl mr-4 fill-dark-gray'} />
           <span className={'text-bold text-dark-gray text-lg'} onClick={() => navigate('/login')}>
             로그인 •
