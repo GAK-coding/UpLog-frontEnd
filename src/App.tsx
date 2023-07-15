aed2c910d45ee1b1267994da45396da2ce76176a
5hyun <qhslsl@gmail.com>
2023-07-15 23:47:57 +0900

    Feat: 비밀번호 찾기 페이지 UI 완성

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import Post from './pages/Post';
 import { ChakraProvider } from '@chakra-ui/react';
+import PwInquiry from './pages/PwInquiry.tsx';
 
 const queryClient = new QueryClient();
 
@@ -30,6 +31,7 @@ function App() {
                 <Route path={'/login'} element={<Login />} />
                 <Route path={'/signup'} element={<SignUp />} />
                 <Route path={'/post'} element={<Post />} />
+                <Route path={'/pwinquiry'} element={<PwInquiry />} />
               </Routes>
             </section>
           </BrowserRouter>
