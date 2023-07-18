f310309a203a862ceb3a20cc9ffccf98185f04c9
5hyun <qhslsl@gmail.com>
2023-07-18 15:00:11 +0900

    Feat: 비밀번호 변경 모달 컴포넌트로 분리

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,22 +1,10 @@
 import React, { useState } from 'react';
-import { RcFile, UploadChangeParam } from 'antd/es/upload';
-import message from 'antd/lib/message';
+import { RcFile } from 'antd/es/upload';
 import { UploadFile, UploadProps } from 'antd/lib';
-import { Button, Upload } from 'antd';
-import {
-  Flex,
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  ModalOverlay,
-  useDisclosure,
-} from '@chakra-ui/react';
+import { Upload } from 'antd';
 import ImgCrop from 'antd-img-crop';
-import { theme } from 'twin.macro';
-import { Link } from 'react-router-dom';
+import { useDisclosure } from '@chakra-ui/react';
+import ChangePwModal from '../components/MyPage/ChangePwModal.tsx';
 
 export default function MyPage() {
   // 비밀번호 변경 모달
@@ -92,8 +80,7 @@ export default function MyPage() {
               <span className={'text-gray-dark text-[0.93rem] font-bold mb-4'}>이름</span>
               <input
                 className={
-                  // TODO: border-gray 뭐임!!!
-                  'border-base border-gray w-full h-10 rounded-[0.625rem] text-[0.93rem] font-bold p-2'
+                  'border-base border-gray-border w-full h-10 rounded-[0.625rem] text-[0.93rem] font-bold p-2'
                 }
                 type="text"
                 maxLength={10}
@@ -104,8 +91,7 @@ export default function MyPage() {
               <span className={'text-gray-dark text-[0.93rem] font-bold mb-4'}>닉네임</span>
               <input
                 className={
-                  // TODO: border-gray 뭐임!!!
-                  'border-base border-gray w-full h-10 rounded-[0.625rem] text-[0.93rem] font-bold p-2'
+                  'border-base border-gray-border w-full h-10 rounded-[0.625rem] text-[0.93rem] font-bold p-2'
                 }
                 type="text"
                 maxLength={10}
@@ -141,51 +127,7 @@ export default function MyPage() {
           </div>
         </div>
       </article>
-
-      <Modal isCentered onClose={onClose} isOpen={true}>
-        <ModalOverlay />
-        <ModalContent
-          maxW="37.5rem"
-          h={'27rem'}
-          shadow={'boxShadow-sign-up'}
-          rounded={'none'}
-          p={'1.2rem'}
-        >
-          <ModalHeader fontSize={'1.8rem'} fontWeight={700}>
-            비밀번호 변경
-          </ModalHeader>
-          <ModalCloseButton
-            fontSize={'1rem'}
-            color={'var(--gray-light)'}
-            mt={'0.6rem'}
-            mr={'0.8rem'}
-          />
-          <ModalBody>
-            <Flex justifyContent={'center'} h={'100%'}>
-              <section className={'border-base flex-row-center justify-start w-[22.5rem] h-full'}>
-                {/* 현재 비밀번호 */}
-                <div>
-                  <span className={'text-[0.93rem] text-gray-dark'}>현재 비밀번호</span>
-                  <input type="password" className={'text-xl tracking-[-0.3rem] border-gray'} />
-                  <div>
-                    <span>비밀번호를 잊으셨나요?</span>
-                    <Link to={'/pwinquiry'}>비밀번호 찾기</Link>
-                  </div>
-                </div>
-
-                {/* 새로운 비밀번호 */}
-                <div></div>
-              </section>
-            </Flex>
-          </ModalBody>
-
-          <ModalFooter>
-            <button className={'bg-orange w-[4.5rem] h-9 rounded font-bold text-xs text-white'}>
-              확인
-            </button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+      <ChangePwModal isOpen={isOpen} onClose={onClose} />
     </section>
   );
 }
