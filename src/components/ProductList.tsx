a0b1f834678681768858ff04d1e7af8358e5f892
CHCHAENG <oco6029@naver.com>
2023-07-21 00:31:49 +0900

    Feat : dnd library Drag, Drop 감싸기까지는 완료

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -4,7 +4,7 @@ import { productOpen } from '../recoil/Product/atom.tsx';
 import { useRecoilState } from 'recoil';
 import { RxDragHandleDots2 } from 'react-icons/rx';
 import { BiPencil } from 'react-icons/bi';
-import { DragDropContext } from 'react-beautiful-dnd';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 
 export default function ProductList() {
   const productList = [
@@ -41,6 +41,10 @@ export default function ProductList() {
     e.stopPropagation();
   };
 
+  const onDragEnd = useCallback((result: any) => {
+    // TODO: 작성필요
+  }, []);
+
   return (
     <section
       className={
@@ -51,21 +55,44 @@ export default function ProductList() {
       <div className={'max-h-[26.7rem] overflow-y-auto'}>
         {/*제품 list*/}
         <DragDropContext onDragEnd={onDragEnd}>
-        {productList.map((product, index) => {
-          return (
-            <div
-              key={index}
-              className={'flex-row-center justify-between w-full h-[4.5rem] hover:bg-hover'}
-            >
-              <RxDragHandleDots2
-                className={'flex w-[2.6rem] items-center text-4xl ml-4 fill-gray-light'}
-              />
-              <img src={product.img} alt="userprofile" className={'ml-2 w-[2rem] h-[2rem]'} />
-              <span className={'ml-3 text-xl font-bold w-full'}>{product.name}</span>
-              <BiPencil className={'w-20 text-xl mr-4 fill-gray-light'} />
-            </div>
-          );
-        })}
+          {productList.map((product, index) => {
+            return (
+              <Droppable droppableId="inbox-column">
+                {(provided) => (
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.droppableProps}
+                    key={index}
+                    className={'flex-row-center justify-between w-full h-[4.5rem] hover:bg-hover'}
+                  >
+                    <Draggable draggableId={productList[0].name} index={index}>
+                      {(provided) => (
+                        <div
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          className={'flex-row-center justify-between w-full h-[4.5rem]'}
+                        >
+                          <RxDragHandleDots2
+                            className={'flex w-[2.6rem] items-center text-4xl ml-4 fill-gray-light'}
+                          />
+                          <img
+                            src={product.img}
+                            alt="userprofile"
+                            className={'ml-2 w-[2rem] h-[2rem]'}
+                          />
+                          <span className={'ml-3 text-xl font-bold w-full'}>{product.name}</span>
+                          <BiPencil className={'w-20 text-xl mr-4 fill-gray-light'} />
+                        </div>
+                      )}
+                    </Draggable>
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+            );
+          })}
+        </DragDropContext>
       </div>
 
       {/*제품 추가하기*/}
