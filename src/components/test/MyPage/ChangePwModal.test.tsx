d078dd9b475aa4cc4f01017072bae2eac2193956
5hyun <qhslsl@gmail.com>
2023-07-21 20:31:19 +0900

    Test: mypage에서 사용하는 모달 테스트 완료

--- src/components/test/MyPage/ChangePwModal.test.tsx
+++ src/components/test/MyPage/ChangePwModal.test.tsx
@@ -1,11 +1,12 @@
 import { describe, expect, test } from '@jest/globals';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, getByText, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { withRouter } from '../../../tests/utils';
 import { Route } from 'react-router-dom';
 import '@testing-library/jest-dom/extend-expect';
 import renderer from 'react-test-renderer';
 import ChangePwModal from '../../MyPage/ChangePwModal';
+import userEvent from '@testing-library/user-event';
 
 describe('ChangePwModal', () => {
   const misPw = 'qwer234';
@@ -23,67 +24,76 @@ describe('ChangePwModal', () => {
     onClose: onClose,
     isClickPwChange: false,
   };
+
   // 테스트가 끝날때 마다 Mock 함수를 깨끗하게 정리
   afterEach(() => onClose.mockReset());
 
+  // 비밀번호 변경 모달
   test('password change modal correctly', () => {
     const component = renderer.create(
-      withRouter(<Route path={'/mypage'} element={<ChangePwModal {...props_pwChange} />} />)
+      withRouter(<Route path={'/'} element={<ChangePwModal {...props_pwChange} />} />, '/mypage')
     );
 
     expect(component.toJSON()).toMatchSnapshot();
   });
 
   test('password change modal, test password', () => {
-    render(withRouter(<Route path={'/mypage'} element={<ChangePwModal {...props_pwChange} />} />));
+    render(
+      withRouter(
+        <Route path={'/mypage'} element={<ChangePwModal {...props_pwChange} />} />,
+        '/mypage'
+      )
+    );
 
-    expect(screen.getByText('현재 비밀번호')).toBeInTheDocument();
+    const passwordInput = screen.getByPlaceholderText('새로운 비밀번호를 입력하세요.');
+
+    fireEvent.change(passwordInput, { target: { value: misPw } });
+    expect(screen.getByText('영문/숫자/특수문자 포함, 8~15자로 입력해주세요.')).toBeInTheDocument();
+
+    fireEvent.change(passwordInput, { target: { value: goodPw } });
+    expect(
+      screen.queryByText('영문/숫자/특수문자 포함, 8~15자로 입력해주세요.')
+    ).not.toBeInTheDocument();
   });
 
+  test('move page, find password button click to /pwinquiry', () => {
+    render(
+      withRouter(
+        <>
+          <Route path={'/mypage'} element={<ChangePwModal {...props_pwChange} />} />
+          <Route path={'/pwinquiry'} element={<p>비밀번호 찾기 페이지</p>} />
+        </>,
+        '/mypage'
+      )
+    );
+
+    const findPwLink = screen.getByRole('link', { name: '비밀번호 찾기' });
+    fireEvent.click(findPwLink);
+    expect(screen.getByText('비밀번호 찾기 페이지')).toBeInTheDocument();
+  });
+
+  // 회원 탈퇴 모달
   test('Member Withdrawal modal correctly', () => {
     const component = renderer.create(
-      withRouter(<Route path={'/mypage'} element={<ChangePwModal {...props_memberWithdrawal} />} />)
+      withRouter(
+        <Route path={'/mypage'} element={<ChangePwModal {...props_memberWithdrawal} />} />,
+        'mypage'
+      )
     );
 
     expect(component.toJSON()).toMatchSnapshot();
   });
 
-  // test('validates the form submission', () => {
-  //   render(withRouter(<Route path={'/'} element={<ChangePwModal />} />));
-  //
-  //   const nameInput = screen.getByPlaceholderText('이름');
-  //   const nickNameInput = screen.getByPlaceholderText('닉네임');
-  //   const emailInput = screen.getByPlaceholderText('이메일');
-  //   const authInput = screen.getByPlaceholderText('인증번호');
-  //   const passwordInput = screen.getByPlaceholderText('비밀번호');
-  //   // const submitButton = screen.getByText('회원가입');
-  //
-  //   fireEvent.change(nameInput, { target: { value: 'John Doe' } });
-  //   fireEvent.change(nickNameInput, { target: { value: 'john_doe' } });
-  //   fireEvent.change(emailInput, { target: { value: 'john.doe@example.com' } });
-  //   fireEvent.change(authInput, { target: { value: '123456' } });
-  //   fireEvent.change(passwordInput, { target: { value: 'Passw0rd!' } });
-  //
-  //   // Click the 인증번호 전송 button
-  //   const sendAuthButton = screen.getByText('전송');
-  //   fireEvent.click(sendAuthButton);
-  //
-  //   // Click the 확인 button to complete the 인증번호 confirmation
-  //   const confirmAuthButton = screen.getByText('확인');
-  //   fireEvent.click(confirmAuthButton);
-  // });
-  //
-  // test('check password', () => {
-  //   render(withRouter(<Route path={'/'} element={<ChangePwModal />} />));
-  //
-  //   const passwordInput = screen.getByPlaceholderText('비밀번호');
-  //   fireEvent.change(passwordInput, { target: { value: misPw } });
-  //
-  //   expect(screen.getByText('영어/숫자/특수문자 포함, 8~15자로 입력해주세요.')).toBeInTheDocument();
-  //
-  //   fireEvent.change(passwordInput, { target: { value: goodPw } });
-  //   expect(
-  //     screen.queryByText('영어/숫자/특수문자 포함, 8~15자로 입력해주세요.')
-  //   ).not.toBeInTheDocument();
-  // });
+  test('check Withdrawal modal screen', () => {
+    render(
+      withRouter(
+        <Route path={'/mypage'} element={<ChangePwModal {...props_memberWithdrawal} />} />,
+        '/mypage'
+      )
+    );
+
+    expect(screen.getByText('현재 비밀번호')).toBeInTheDocument();
+    // getByText는 존재하지 않으명 에러가 나는데, queryByText는 존재하지 않으면 애러가 안나서 not을 하고 싶으면 queryByT
+    expect(screen.queryByText('새로운 비밀번호')).not.toBeInTheDocument();
+  });
 });
