3df260badeec909064676ca8e773bb313efd3fbc
CHCHAENG <oco6029@naver.com>
2023-07-25 11:12:37 +0900

    Feat : antd select custom 하는 중

---
+++ src/components/UI/Select.tsx
@@ -0,0 +1,14 @@
+interface Props {
+  width: string;
+  items: {
+    value: string;
+    label: string;
+  }[];
+}
+export default function CustomSelect(props: Props) {
+  return (
+    <div>
+      <div>CustomSelect</div>
+    </div>
+  );
+}
