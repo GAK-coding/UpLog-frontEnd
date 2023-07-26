fed5b39a1976c123f0b3f09a777f1527a1b82922
5hyun <qhslsl@gmail.com>
2023-07-26 21:34:47 +0900

    Style: 사이드바 채팅 부분 제외하고 구현 완료

--- src/layouts/Workspace.tsx
+++ src/layouts/Workspace.tsx
@@ -5,6 +5,7 @@ import { BsChatDots } from 'react-icons/bs';
 import { SlCalender } from 'react-icons/sl';
 import Chats from '@/components/Product/SideBar/Chats.tsx';
 import Groups from '@/components/Product/SideBar/Groups.tsx';
+import { Scrollbars } from 'rc-scrollbars';
 
 export default function Workspace() {
   const { product } = useParams();
@@ -12,8 +13,8 @@ export default function Workspace() {
 
   return (
     <section className={'w-h-full flex '}>
-      <nav className={'w-[18.6rem] h-full border-solid border-r border-header-gray'}>
-        <div className={'h-80 border-solid border-b border-header-gray'}>
+      <nav className={'w-[18.6rem] h-full border-r border-gray-sideBar]'}>
+        <div className={'h-[34%] border-b'}>
           <div className={'h-1/4 flex-row-center'}>
             <span className={'text-3xl font-bold mr-4'}>AllFromU</span>
             <span className={'text-gray-dark text-xl font-bold'}>v.1.0.0</span>
@@ -21,40 +22,35 @@ export default function Workspace() {
           <div className={'h-3/4 flex flex-col justify-evenly'}>
             <NavLink
               to={`/workspace/${product}/members`}
-              className={({ isActive }) =>
-                `text-[1.4rem] flex items-center font-bold text-gray-dark w-56 h-12  mx-auto  ${
-                  isActive &&
-                  'border-solid border-[0.5px] border-orange-light-sideBar bg-orange-light-sideBar rounded-[0.31rem]'
-                }`
-              }
+              className={({ isActive }) => `base-menu ${isActive && 'base-menu-selected'}`}
             >
               <FiUser className={'text-[1.8rem] ml-2 mr-3'} /> 멤버
             </NavLink>
             <NavLink
               to={`/workspace/${product}/chats`}
-              className={({ isActive }) =>
-                `text-[1.4rem] flex items-center font-bold text-gray-dark w-56 h-12  mx-auto  ${
-                  isActive &&
-                  'border-solid border-[0.5px] border-orange-light-sideBar bg-orange-light rounded-[0.31rem]'
-                }`
-              }
+              className={({ isActive }) => `base-menu ${isActive && 'base-menu-selected'}`}
             >
               <BsChatDots className={'text-[1.8rem] ml-2 mr-3'} /> 채팅방
             </NavLink>
             <NavLink
               to={`/workspace/${product}/calendar`}
-              className={({ isActive }) =>
-                `text-[1.4rem] flex items-center font-bold text-gray-dark w-56 h-12  mx-auto  ${
-                  isActive &&
-                  'border-solid border-[0.5px] border-orange-light-sideBar bg-orange-light rounded-[0.31rem]'
-                }`
-              }
+              className={({ isActive }) => `base-menu ${isActive && 'base-menu-selected'}`}
             >
               <SlCalender className={'text-[1.8rem] ml-2 mr-3'} /> 캘린더
             </NavLink>
           </div>
         </div>
-        <div>{pathname === `/workspace/${product}/chats` ? <Chats /> : <Groups />}</div>
+        <div className={'overflow-y-auto h-[66%]'}>
+          <Scrollbars
+            style={{ height: '100%' }}
+            autoHide
+            autoHideTimeout={1000}
+            // Duration for hide animation in ms.
+            autoHideDuration={200}
+          >
+            {pathname === `/workspace/${product}/chats` ? <Chats /> : <Groups />}
+          </Scrollbars>
+        </div>
       </nav>
       <article className={'w-noneSideBar h-full'}>
         {/* 중첩 라우터 */}
