ab3a74319d5561cf82e281e5963edb7365e5e864
5hyun <qhslsl@gmail.com>
2023-07-28 21:23:24 +0900

    Feat: 릴리즈 노트 페이지 개발 중

---
+++ src/components/Product/ReleaseNote/Tables.tsx
@@ -0,0 +1,193 @@
+import React, { useCallback, useState } from 'react';
+import { Tbody, Td, Tr } from '@chakra-ui/react';
+import { changeType, Release } from '@/typings/product.ts';
+import { GoKebabHorizontal } from 'react-icons/go';
+
+export default function Tables() {
+  const bgColor: Record<changeType, string> = {
+    Feature: 'bg-type-feature',
+    Changed: 'bg-type-changed',
+    Fixed: 'bg-type-fixed',
+    New: 'bg-type-new',
+    Deprecated: 'bg-type-deprecated',
+  };
+
+  // TODO: 리코일에서 받아 올 값
+  const dummy: Release[] = [
+    {
+      status: 'going',
+      version: 'v.1.1.3(임시)',
+      date: '진행중',
+      contents: [
+        { type: 'Feature', content: '채널 통신 응답 중 발생할 수 있는 오류 코드 추가' },
+        {
+          type: 'Changed',
+          content: '공통 Request Protocol',
+        },
+        {
+          type: 'Fixed',
+          content: '고침',
+        },
+        {
+          type: 'New',
+          content: '새거',
+        },
+        {
+          type: 'Deprecated',
+          content: '이제 못 씀',
+        },
+      ],
+    },
+    // {
+    //   status: 'done',
+    //   version: 'v.1.1.2',
+    //   date: '2023.06.12',
+    //   contents: [
+    //     { type: 'Feature', content: '채널 통신 응답 중 발생할 수 있는 오류 코드 추가' },
+    //     {
+    //       type: 'Changed',
+    //       content: '공통 Request Protocol 추가',
+    //     },
+    //   ],
+    // },
+    // {
+    //   status: 'done',
+    //   version: 'v.1.1.1',
+    //   date: '2023.05.12',
+    //   contents: [{ type: 'Deprecated', content: '미사용 필드 삭제' }],
+    // },
+    // {
+    //   status: 'done',
+    //   version: 'v.1.1.0',
+    //   date: '2023.03.25',
+    //   contents: [{ type: 'Feature', content: 'Service Argent 채널 연결과 통신 관련 설명' }],
+    // },
+    {
+      status: 'done',
+      version: 'v.1.0.0',
+      date: '2023.02.18',
+      contents: [
+        { type: 'New', content: 'AI Service 기술 상세 설명' },
+        { type: 'Fixed', content: '채널 통신 응답 기능 수정' },
+      ],
+    },
+  ];
+
+  const [isClick, setIsClick] = useState(false);
+  const [isHovering, setIsHovering] = useState(false);
+
+  const onClickModal = useCallback((e: React.MouseEvent<HTMLElement>) => {
+    e.stopPropagation();
+
+    setIsClick((prev) => !prev);
+  }, []);
+
+  const onCloseModal = useCallback(() => {
+    setIsClick(false);
+  }, []);
+
+  const handleMouseEnter = useCallback((status: string) => {
+    if (status === 'going') setIsHovering(true);
+  }, []);
+
+  const handleMouseLeave = useCallback((status: string) => {
+    if (status === 'going') setIsHovering(false);
+    setIsClick(false);
+  }, []);
+
+  return (
+    <Tbody>
+      {dummy.map((version, index) => {
+        return (
+          <Tr
+            key={index}
+            color={version.status === 'going' ? 'var(--gray-dark)' : ''}
+            position={'relative'}
+            onMouseEnter={() => handleMouseEnter(version.status)}
+            onMouseLeave={() => handleMouseLeave(version.status)}
+            onClick={onCloseModal}
+          >
+            <Td
+              borderTop={'1px solid var(--gray-table)'}
+              borderBottom={'1px solid var(--gray-table)'}
+              padding={'1.6rem 0'}
+              textAlign={'center'}
+            >
+              {version.version}
+            </Td>
+            <Td
+              borderTop={'1px solid var(--gray-table)'}
+              borderBottom={'1px solid var(--gray-table)'}
+              textAlign={'center'}
+            >
+              {version.date}
+            </Td>
+            <Td
+              borderTop={'1px solid var(--gray-table)'}
+              borderBottom={'1px solid var(--gray-table)'}
+              marginY={'auto'}
+            >
+              {/* Contents of the table row */}
+              {version.contents.map((content, idx) => {
+                return (
+                  <div
+                    key={idx}
+                    className={`${version.contents.length - 1 !== idx && 'mb-5'} flex items-center`}
+                  >
+                    <span
+                      className={`${
+                        bgColor[content.type]
+                      } text-[#292723] mr-4 p-2 rounded-[0.31rem]`}
+                    >
+                      {content.type}
+                    </span>
+                    {content.content}
+                  </div>
+                );
+              })}
+            </Td>
+
+            {/* Buttons for the "going" status */}
+            {version.status === 'going' && isHovering && (
+              <Td
+                position={'absolute'}
+                top={'-0.5rem'}
+                right={'-1rem'}
+                backgroundColor={'inherit'}
+                border={'none'}
+              >
+                <button onClick={(e) => onClickModal(e)}>
+                  <GoKebabHorizontal className={'fill-gray-dark text-2xl cursor-pointer'} />
+                </button>
+                {isClick && (
+                  <div
+                    className={
+                      'absolute top-10 right-6 flex flex-col w-[7.5rem] h-20 border-solid border-[0.5px] border-gray-spring rounded text-xs shadow-release bg-white'
+                    }
+                  >
+                    <button
+                      className={'h-1/2 hover:bg-orange-light'}
+                      onClick={() => {
+                        console.log('클릭');
+                      }}
+                    >
+                      변경이력 추가
+                    </button>
+                    <button
+                      className={'h-1/2 hover:bg-orange-light'}
+                      onClick={() => {
+                        console.log('완료');
+                      }}
+                    >
+                      완료
+                    </button>
+                  </div>
+                )}
+              </Td>
+            )}
+          </Tr>
+        );
+      })}
+    </Tbody>
+  );
+}
--- src/pages/Product/ReleaseNote.tsx
+++ src/pages/Product/ReleaseNote.tsx
@@ -1,20 +1,73 @@
-import React from 'react';
-import {
-  Table,
-  TableCaption,
-  TableContainer,
-  Tbody,
-  Td,
-  Tfoot,
-  Th,
-  Thead,
-  Tr,
-} from '@chakra-ui/react';
-
+import React, { useCallback, useState } from 'react';
+import { Release } from '@/typings/product.ts';
+import { Table, TableContainer, Th, Thead, Tr } from '@chakra-ui/react';
+import { AiOutlinePlus } from 'react-icons/ai';
+import Tables from '@/components/Product/ReleaseNote/Tables.tsx';
 export default function ReleaseNote() {
+  // TODO: 리코일에서 받아 올 값
+  const dummy: Release[] = [
+    {
+      status: 'going',
+      version: 'v.1.1.3(임시)',
+      date: '진행중',
+      contents: [
+        { type: 'Feature', content: '채널 통신 응답 중 발생할 수 있는 오류 코드 추가' },
+        {
+          type: 'Changed',
+          content: '공통 Request Protocol',
+        },
+        {
+          type: 'Fixed',
+          content: '고침',
+        },
+        {
+          type: 'New',
+          content: '새거',
+        },
+        {
+          type: 'Deprecated',
+          content: '이제 못 씀',
+        },
+      ],
+    },
+    // {
+    //   status: 'done',
+    //   version: 'v.1.1.2',
+    //   date: '2023.06.12',
+    //   contents: [
+    //     { type: 'Feature', content: '채널 통신 응답 중 발생할 수 있는 오류 코드 추가' },
+    //     {
+    //       type: 'Changed',
+    //       content: '공통 Request Protocol 추가',
+    //     },
+    //   ],
+    // },
+    // {
+    //   status: 'done',
+    //   version: 'v.1.1.1',
+    //   date: '2023.05.12',
+    //   contents: [{ type: 'Deprecated', content: '미사용 필드 삭제' }],
+    // },
+    // {
+    //   status: 'done',
+    //   version: 'v.1.1.0',
+    //   date: '2023.03.25',
+    //   contents: [{ type: 'Feature', content: 'Service Argent 채널 연결과 통신 관련 설명' }],
+    // },
+    {
+      status: 'done',
+      version: 'v.1.0.0',
+      date: '2023.02.18',
+      contents: [
+        { type: 'New', content: 'AI Service 기술 상세 설명' },
+        { type: 'Fixed', content: '채널 통신 응답 기능 수정' },
+      ],
+    },
+  ];
+
   return (
-    <section className={'w-full py-32 px-56 '}>
-      <div className={'border border-solid flex justify-between mb-4'}>
+    <section className={'w-full py-32 px-56'}>
+      <div className={'flex justify-between mb-4'}>
         <span className={'flex items-center'}>
           <img src="/images/test.jpeg" alt={'제품 사진'} className={'w-14 h-14 mr-4'} />
           <span className={'text-[2.6rem] font-bold'}>AllFormU</span>
@@ -23,42 +76,53 @@ export default function ReleaseNote() {
           프로젝트 추가하기
         </button>
       </div>
+
       <div>
-        <TableContainer>
-          <Table variant="simple">
-            <TableCaption>Imperial to metric conversion factors</TableCaption>
+        <TableContainer overflow={'hidden'}>
+          <Table fontSize={'1.2rem'} fontWeight={700}>
             <Thead>
               <Tr>
-                <Th>To convert</Th>
-                <Th>into</Th>
-                <Th isNumeric>multiply by</Th>
+                <Th
+                  borderTop={'1px solid var(--gray-table)'}
+                  borderBottom={'1px solid var(--gray-table)'}
+                  fontSize={'1.2rem'}
+                  padding={'1.6rem 0'}
+                  textAlign={'center'}
+                  width={'20%'}
+                >
+                  버전
+                </Th>
+                <Th
+                  borderTop={'1px solid var(--gray-table)'}
+                  borderBottom={'1px solid var(--gray-table)'}
+                  fontSize={'1.2rem'}
+                  padding={'1.6rem 0'}
+                  textAlign={'center'}
+                  width={'20%'}
+                >
+                  날짜
+                </Th>
+                <Th
+                  borderTop={'1px solid var(--gray-table)'}
+                  borderBottom={'1px solid var(--gray-table)'}
+                  fontSize={'1.2rem'}
+                  padding={'1.6rem 0'}
+                  textAlign={'center'}
+                  width={'60%'}
+                >
+                  변경이력
+                </Th>
               </Tr>
             </Thead>
-            <Tbody>
-              <Tr>
-                <Td>inches</Td>
-                <Td>millimetres (mm)</Td>
-                <Td isNumeric>25.4</Td>
-              </Tr>
-              <Tr>
-                <Td>feet</Td>
-                <Td>centimetres (cm)</Td>
-                <Td isNumeric>30.48</Td>
-              </Tr>
-              <Tr>
-                <Td>yards</Td>
-                <Td>metres (m)</Td>
-                <Td isNumeric>0.91444</Td>
-              </Tr>
-            </Tbody>
-            <Tfoot>
-              <Tr>
-                <Th>To convert</Th>
-                <Th>into</Th>
-                <Th isNumeric>multiply by</Th>
-              </Tr>
-            </Tfoot>
+            {dummy.length > 0 && <Tables />}
           </Table>
+          {dummy.length === 0 && (
+            <div
+              className={'flex-row-center mt-6 text-gray-light text-2xl font-bold cursor-pointer'}
+            >
+              <AiOutlinePlus className={'mr-4 text-3xl'} /> 프로젝트 시작하기
+            </div>
+          )}
         </TableContainer>
       </div>
     </section>
