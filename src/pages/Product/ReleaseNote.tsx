1f836647bdc262c79b55d80aa05f6c5ab5494d50
5hyun <qhslsl@gmail.com>
2023-07-31 00:27:30 +0900

    Style: 글자 및 사소한 스타일 수정

--- src/pages/Product/ReleaseNote.tsx
+++ src/pages/Product/ReleaseNote.tsx
@@ -97,8 +97,8 @@ export default function ReleaseNote() {
   }, []);
 
   return (
-    <section className={'w-h-full py-32 px-56'} onClick={onCloseKebab}>
-      <div className={'flex justify-between mb-4'}>
+    <section className={'w-h-full min-w-[50em] py-32 px-14 xl:px-56'} onClick={onCloseKebab}>
+      <div className={'min-w-[30em] flex justify-between mb-4'}>
         <span className={'flex items-center'}>
           <img src="/images/test.jpeg" alt={'제품 사진'} className={'w-14 h-14 mr-4'} />
           <span className={'text-[2.6rem] font-bold'}>AllFormU</span>
