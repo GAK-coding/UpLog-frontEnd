7dfc9195323b8df70b949985598fdc657cb0b6be
CHCHAENG <oco6029@naver.com>
2023-08-01 16:18:50 +0900

    Feat : 하위그룹으로 Task 데이터 필터링 완료

--- src/pages/Project/Project.tsx
+++ src/pages/Project/Project.tsx
@@ -5,21 +5,20 @@ import { Task } from '@/typings/project.ts';
 import { Progress, Select, Space } from 'antd';
 import StatusBoard from '@/components/Project/Board/StatusBoard.tsx';
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
-import type = Mocha.utils.type;
 import { useNavigate } from 'react-router-dom';
-import { product } from '@/recoil/Product/atom.tsx';
 
 export default function Project() {
-  const { product, project, parentgroup, childgroup } = useParams();
+  const { product, project, group, subGroup } = useParams();
 
   const navigate = useNavigate();
 
+  // recoil에서 받아올 값
   const taskList: Task[] = [
     {
       id: 0,
       name: 'task1',
       status: 'before',
-      group: 'develop',
+      group: '개발팀',
       menu: '요구사항',
       targetMember: 'OCI(오채영)',
     },
@@ -27,7 +26,7 @@ export default function Project() {
       id: 1,
       name: 'task2',
       status: 'before',
-      group: 'develop',
+      group: '개발팀',
       menu: '요구사항',
       targetMember: 'OCI(오채영)',
     },
@@ -35,7 +34,7 @@ export default function Project() {
       id: 2,
       name: 'task3',
       status: 'before',
-      group: 'develop',
+      group: '마케팅팀',
       menu: '요구사항',
       targetMember: 'OCI(오채영)',
     },
@@ -43,7 +42,7 @@ export default function Project() {
       id: 3,
       name: 'task4',
       status: 'going',
-      group: 'develop',
+      group: '마케팅팀',
       menu: '테스트',
       targetMember: 'OCI(오채영)',
     },
@@ -51,7 +50,7 @@ export default function Project() {
       id: 4,
       name: 'task5',
       status: 'going',
-      group: 'develop',
+      group: '홍보팀',
       menu: '테스트',
       targetMember: 'OCI(오채영)',
     },
@@ -59,7 +58,7 @@ export default function Project() {
       id: 5,
       name: 'task6',
       status: 'going',
-      group: 'develop',
+      group: '홍보팀',
       menu: '요구사항',
       targetMember: 'OCI(오채영)',
     },
@@ -67,7 +66,7 @@ export default function Project() {
       id: 6,
       name: 'task7',
       status: 'done',
-      group: 'develop',
+      group: '개발팀',
       menu: '요구사항',
       targetMember: 'OCI(오채영)',
     },
@@ -75,20 +74,53 @@ export default function Project() {
       id: 7,
       name: 'task8',
       status: 'before',
-      group: 'develop',
+      group: '마케팅',
       menu: '요구사항',
       targetMember: 'OCI(오채영)',
     },
     {
       id: 8,
       name: 'task9',
+      status: 'done',
+      group: '백엔드',
+      menu: '테스트',
+      targetMember: 'OCI(오채영)',
+    },
+    {
+      id: 9,
+      name: 'task10',
+      status: 'done',
+      group: '프론트엔드',
+      menu: '요구사항',
+      targetMember: 'OCI(오채영)',
+    },
+    {
+      id: 10,
+      name: 'task11',
       status: 'before',
-      group: 'develop',
+      group: '프론트엔드',
+      menu: '테스트',
+      targetMember: 'OCI(오채영)',
+    },
+    {
+      id: 11,
+      name: 'task12',
+      status: 'before',
+      group: '백엔드',
+      menu: '요구사항',
+      targetMember: 'OCI(오채영)',
+    },
+    {
+      id: 12,
+      name: 'task13',
+      status: 'going',
+      group: '프론트엔드',
       menu: '테스트',
       targetMember: 'OCI(오채영)',
     },
   ];
 
+  // 그룹으로 필터링 된 값
   const progress = 77;
   const [isKanban, setIsKanban] = useState(true);
 
@@ -96,12 +128,12 @@ export default function Project() {
     setIsKanban(check);
   }, []);
 
-  const pGroup = ['그룹', '개발팀', '마케팅', '홍보팀'];
+  const pGroup = ['그룹', '개발팀', '마케팅팀', '홍보팀'];
   const cGroup = {
     그룹: ['하위그룹'],
-    개발팀: ['전체', '하위개발1', '하위개발2'],
-    마케팅: ['전체', '하위마케팅1', '하위마케팅2'],
-    홍보팀: ['전체', '하위홍보1', '하위홍보2'],
+    개발팀: ['전체', '프론트엔드', '백엔드', '풀스택'],
+    마케팅팀: ['전체', '콘텐츠', '디자인'],
+    홍보팀: ['전체', 'SNS', '기사'],
   };
 
   type ChildGroup = keyof typeof cGroup;
@@ -110,6 +142,8 @@ export default function Project() {
   const [childGroup, setChildGroup] = useState(cGroup[pGroup[0] as ChildGroup][0]);
 
   const [filterGroup, setFilterGroup] = useState(pGroup[0]);
+
+  const [filterTaskList, setFilterTaskList] = useState(taskList);
 
   const handleParentGroupChange = (value: ChildGroup) => {
     // 선택한 상위그룹내용으로 하위 그룹 option으로 변경
@@ -125,6 +159,7 @@ export default function Project() {
     setChildGroup(value);
   };
 
+  // 필터링 된 페이지로 이동
   useEffect(() => {
     if (filterGroup === '그룹') {
       navigate(`/workspace/${product}/${project}`);
@@ -132,6 +167,20 @@ export default function Project() {
       childGroup === '전체'
         ? navigate(`/workspace/${product}/${project}/group/${filterGroup}`)
         : navigate(`/workspace/${product}/${project}/group/${filterGroup}/${childGroup}`);
+    }
+  }, [filterGroup, childGroup]);
+
+  // task 데이터 필터링된 그룹에 맞게 필터링
+  useEffect(() => {
+    if (filterGroup === undefined) return;
+
+    if (childGroup === '전체') {
+      setFilterTaskList(taskList.filter((task) => task.group === filterGroup));
+      // const allGroup = cGroup[filterGroup];
+      //
+      // console.log(allGroup);
+    } else {
+      setFilterTaskList(taskList.filter((task) => task.group === childGroup));
     }
   }, [filterGroup, childGroup]);
 
@@ -214,9 +263,9 @@ export default function Project() {
       <div className={'w-noneSideBar h-board flex-col'}>
         <section className={'flex-col-center w-noneSideBar h-[90%]'}>
           <div className={'flex-row-center justify-between w-full h-full pt-8 px-[12rem]'}>
-            <StatusBoard status={'before'} tasks={taskList} />
-            <StatusBoard status={'going'} tasks={taskList} />
-            <StatusBoard status={'done'} tasks={taskList} />
+            <StatusBoard status={'before'} tasks={filterTaskList} />
+            <StatusBoard status={'going'} tasks={filterTaskList} />
+            <StatusBoard status={'done'} tasks={filterTaskList} />
           </div>
         </section>
         {/*하단페이지로 이동*/}
