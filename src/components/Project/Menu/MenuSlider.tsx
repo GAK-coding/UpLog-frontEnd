f46b0b85845a2f1925da7bc53d95957b13b9f4e1
CHCHAENG <oco6029@naver.com>
2023-08-03 16:39:40 +0900

    Style : slide 화살표 커스텀 완료

--- src/components/Project/Menu/MenuSlider.tsx
+++ src/components/Project/Menu/MenuSlider.tsx
@@ -3,13 +3,37 @@ import 'slick-carousel/slick/slick-theme.css';
 import Slider from 'react-slick';
 import { MenuInfo } from '@/typings/project.ts';
 import { NavLink } from 'react-router-dom';
-import { IoIosArrowForward } from 'react-icons/io';
+import { IoIosArrowForward, IoIosArrowBack } from 'react-icons/io';
+import styled from '@emotion/styled';
 
 interface Props {
   product: string;
   project: string;
   menuList: MenuInfo[];
 }
+
+const StyledSlider = styled(Slider)`
+  .slick-prev::before,
+  .slick-next::before {
+    opacity: 0;
+    display: none;
+  }
+  .slick-slide div {
+    //슬라이더  컨텐츠
+    cursor: pointer;
+  }
+`;
+
+const CustomPrevSlider = styled.div`
+  left: 3px;
+  z-index: 99;
+`;
+
+const CustomNextSlider = styled.div`
+  right: 3px;
+  z-index: 99;
+`;
+
 export default function MenuSlider({ product, project, menuList }: Props) {
   // slide settings 커스텀
   const settings = {
@@ -18,32 +42,50 @@ export default function MenuSlider({ product, project, menuList }: Props) {
     speed: 800, // 넘어가는 속도
     slidesToShow: 5, // 하나의 슬라이드에서 보여줄 개수
     slidesToScroll: 5, // 슬라이드 넘길 시 몇개의 아이템을 넘길지
+    prevArrow: (
+      <CustomPrevSlider>
+        <IoIosArrowBack className={'text-[2rem] text-gray-border'} />
+      </CustomPrevSlider>
+    ),
+    nextArrow: (
+      <CustomNextSlider>
+        <IoIosArrowForward className={'text-[2rem] text-gray-border'} />
+      </CustomNextSlider>
+    ),
   };
 
   return (
-    <Slider {...settings} className={'w-h-full text-[1.25rem] font-bold text-gray-border'}>
+    <StyledSlider {...settings} className={'w-h-full text-[1.25rem] font-bold text-gray-border'}>
+      {/*결과물 menu (defult)*/}
       <NavLink
         to={`/workspace/${product}/${project}/menu/main`}
         className={({ isActive }) =>
-          `flex-row-center h-[5rem] w-1/5 ${isActive && 'bg-orange text-black'}`
+          `flex-row-center h-[5rem] w-1/5 border-r border-gray-border ${
+            isActive && 'bg-orange text-black'
+          }`
         }
       >
         <span className={'flex-row-center h-full w-full'}>{menuList[0].name}</span>
       </NavLink>
+      {/*menuList 데이터*/}
       {menuList.slice(1).map((menu, index) => (
         <NavLink
           to={`/workspace/${product}/${project}/menu/${menu.name}`}
           className={({ isActive }) =>
-            `flex-row-center h-[5rem] w-1/5 ${isActive && 'bg-orange text-black'}`
+            `flex-row-center h-[5rem] w-1/5 border-r border-gray-border ${
+              isActive && 'bg-orange text-black'
+            }`
           }
           key={index}
         >
           <span className={'flex-row-center h-full w-full'}>{menu.name}</span>
         </NavLink>
       ))}
-      <div className={'flex-row-center h-[5rem] w-1/5'}>
-        <IoIosArrowForward className={'text-[2rem] text-gray-light'} />
-      </div>
-    </Slider>
+      {menuList.length < 15 && (
+        <div className={'flex-row-center h-[5rem] w-1/5'}>
+          <IoIosArrowForward className={'text-[2rem] text-gray-light'} />
+        </div>
+      )}
+    </StyledSlider>
   );
 }
--- src/pages/Project/Menu.tsx
+++ src/pages/Project/Menu.tsx
@@ -1,9 +1,7 @@
-import { NavLink, useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { BsChevronCompactUp } from 'react-icons/bs';
 import { MenuInfo } from '@/typings/project.ts';
 import MenuSlider from '@/components/Project/Menu/MenuSlider.tsx';
-import Slider from 'react-slick';
-import { IoIosArrowForward } from 'react-icons/io';
 
 export default function Menu() {
   const { product, project } = useParams();
@@ -18,28 +16,23 @@ export default function Menu() {
     { id: 6, name: '배포2' },
   ];
 
-  // slide settings 커스텀
-  const settings = {
-    dots: false, // 밑에 점으로 표시되는 것
-    infinite: false, // loop 만들지 결정 여부
-    speed: 800, // 넘어가는 속도
-    slidesToShow: 5, // 하나의 슬라이드에서 보여줄 개수
-    slidesToScroll: 5, // 슬라이드 넘길 시 몇개의 아이템을 넘길지
-  };
-
   return (
     <section className={'flex-col-center justify-start w-noneSideBar h-full'}>
       <section className={'flex-row-center h-[5rem] w-full'}>
+        {/*프로젝트 페이지로 이동하는 버튼*/}
         <BsChevronCompactUp
           className={'text-[4rem] text-gray-light cursor-pointer'}
           onClick={() => navigate(`/workspace/${product}/${project}`)}
         />
       </section>
+      {/*메뉴 보드 Wrapper*/}
       <section className={'flex-col w-[80%] min-w-[80rem] h-menu pt-6'}>
+        {/*post, task 메뉴보드*/}
         <section className={'w-h-full border-base border-b-0 border-gray-border'}>
-          <div className={'flex-row-center w-full h-[5rem] border-b border-gray-border'}>
-            <div className={'w-[90%] min-w-[70rem] h-full items-center justify-center'}>
-              <MenuSlider product={product} project={project} menuList={menuList} />
+          {/*메뉴 list*/}
+          <div className={'flex-row-center w-full h-[5rem] border-b border-gray-border '}>
+            <div className={'w-full min-w-[70rem] h-full items-center justify-center'}>
+              <MenuSlider product={product!} project={project!} menuList={menuList} />
             </div>
           </div>
         </section>
