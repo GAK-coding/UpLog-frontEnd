9b8420d01a8a6e1b47c34c99da868bf22463abdb
CHCHAENG <oco6029@naver.com>
2023-08-05 16:23:16 +0900

    Chore : test 안돼

---
+++ src/components/test/Member/Header/UserProfile.test.tsx
@@ -0,0 +1,41 @@
+import { describe, expect, test } from '@jest/globals';
+import { fireEvent, getByText, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { withAllContexts, withRouter } from '@/tests/utils';
+import { Route, Router } from 'react-router-dom';
+import '@testing-library/jest-dom/extend-expect';
+import renderer from 'react-test-renderer';
+import UserProfile from '@/components/Member/Header/UserProfile';
+import userEvent from '@testing-library/user-event';
+import { RecoilRoot } from 'recoil';
+
+describe('UserProfile', () => {
+  const userInfo = {
+    userprofile: '',
+    userName: '오채영',
+    userNickname: 'OCI',
+    userId: 'oco6029@naver.com',
+  };
+
+  test('UserProfile Modal UI', () => {
+    const component = renderer.create(
+      withRouter(<Route path={'/'} element={<UserProfile />} />, '/workspace/product')
+    );
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  test('UserProfile Modal Open MenuInfo Correctly', () => {
+    render(
+      withAllContexts(
+        withRouter(
+          <Route path={'/workspace/product'} element={<UserProfile />} />,
+          '/workspace/product'
+        )
+      )
+    );
+
+    expect(screen.getByText('스크랩')).toBeInTheDocument();
+    expect(screen.getByText('프로필 수정')).toBeInTheDocument();
+    expect(screen.getByText('로그아웃')).toBeInTheDocument();
+  });
+});
--- src/tests/utils.tsx
+++ src/tests/utils.tsx
@@ -1,9 +1,27 @@
-import {MemoryRouter, Routes} from "react-router-dom";
+import { MemoryRouter, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-export function withRouter(routes: JSX.Element, initialEntry: string = "/"): JSX.Element {
-    return (
-        <MemoryRouter initialEntries={[initialEntry]}>
-            <Routes>{routes}</Routes>
-        </MemoryRouter>
-    );
+function createTestQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: { retry: false },
+    },
+    logger: {
+      log: console.log,
+      warn: console.warn,
+      error: () => {},
+    },
+  });
+}
+export function withRouter(routes: JSX.Element, initialEntry: string = '/'): JSX.Element {
+  return (
+    <MemoryRouter initialEntries={[initialEntry]}>
+      <Routes>{routes}</Routes>
+    </MemoryRouter>
+  );
+}
+
+export function withAllContexts(children: JSX.Element) {
+  const testClient = createTestQueryClient();
+  return <QueryClientProvider client={testClient}>{children}</QueryClientProvider>;
 }
