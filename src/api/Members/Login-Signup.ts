cdcbf0bcf26271156d75f4f6d877979d10aa697d
5hyun <qhslsl@gmail.com>
2023-08-07 16:38:45 +0900

    Feat: axios baseurl 설정 및 회원가입 연동 성공

--- src/api/Members/Login-Signup.ts
+++ src/api/Members/Login-Signup.ts
@@ -1,9 +1,9 @@
-import axios from 'axios';
 import { SignUpInfo } from '@/typings/member.ts';
+import { instance } from '@/api';
 
 export const signUp = async (data: SignUpInfo) => {
   try {
-    const res = await axios.post('http://localhost:8080/members', data);
+    const res = await instance.post('/members', data);
     return res;
   } catch (err) {
     console.error(err);
