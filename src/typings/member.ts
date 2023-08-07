f40bf8b6f880d75e36dd7a434574b481b3bb0a1a
CHCHAENG <oco6029@naver.com>
2023-08-07 21:18:51 +0900

    Chore : develop merge

--- src/typings/member.ts
+++ src/typings/member.ts
@@ -1,8 +1,19 @@
-export interface SignUpInfo {
+export interface LoginInfo {
   email: string;
   password: string;
+}
+
+export interface SignUpInfo extends LoginInfo {
   nickname: string;
   name: string;
   position: 'INDIVIDUAL' | 'COMPANY';
   loginType: 'UPLOG' | 'GOOGLE';
 }
+
+export interface GetUserInfo extends Omit<SignUpInfo, 'loginType' | 'password'> {
+  id: number;
+  accessToken: string;
+  refreshToken: string;
+}
+
+export interface SaveUserInfo extends Omit<GetUserInfo, 'accessToken' | 'refreshToken'> {}
