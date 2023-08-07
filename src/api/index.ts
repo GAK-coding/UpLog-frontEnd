50283d615749f9a037745d2797f570b8a8735221
5hyun <qhslsl@gmail.com>
2023-08-08 01:41:53 +0900

    Feat: jwt 자동 삽임

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,6 +5,8 @@ const DEV_BACKEND_URL = import.meta.env.VITE_DEV_BACKEND_URL;
 const DEPLOYMENT_BACKEND_URL = import.meta.env.VITE_DEPLOYMENT_BACKEND_URL;
 
 const URL = isDeployment ? DEPLOYMENT_BACKEND_URL : DEV_BACKEND_URL;
+const jwt = sessionStorage.getItem('accessToken');
 export const instance = axios.create({
   baseURL: URL,
+  headers: { Authorization: `Bearer ${jwt}` },
 });
