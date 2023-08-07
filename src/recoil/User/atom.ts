592f13560e62a8d56f776f3137600dd6bbb273c3
5hyun <qhslsl@gmail.com>
2023-08-08 01:30:36 +0900

    Feat: 로그인 상태에 따른 헤더 변경

--- src/recoil/User/atom.ts
+++ src/recoil/User/atom.ts
@@ -1,3 +1,4 @@
-import {atom} from "recoil";
+import { atom } from 'recoil';
 
+export const loginStatus = atom({ key: 'loginStatus', default: false });
 export const profileOpen = atom({ key: 'profileOpen', default: false });
