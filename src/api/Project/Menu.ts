8138101425e8305cdf43b1db61548826e3d126c2
CHCHAENG <oco6029@naver.com>
2023-08-11 19:22:07 +0900

    Feat : Menu C,U,D axios 코드 작성

---
+++ src/api/Project/Menu.ts
@@ -0,0 +1,53 @@
+import { AxiosResponse } from 'axios';
+import { MenuInfo } from '@/typings/menu.ts';
+import { instance } from '@/api';
+
+// 프로젝트에 해당하는 메뉴 리스트 get
+export const projectMenuList = async (projectId: number) => {
+  try {
+    const res: AxiosResponse<MenuInfo> = await instance.get(`/menus/${projectId}`);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    return 'get project menu list fail';
+  }
+};
+
+// 메뉴 생성
+export const createMenu = async (projectId: string, menuName: string) => {
+  try {
+    const res: AxiosResponse<MenuInfo> = await instance.post(`/menus/${projectId}`, menuName);
+
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    return 'create menu fail';
+  }
+};
+
+// 메뉴 이름 수정
+export const editMenu = async (menuId: number, updatemenuName: string) => {
+  try {
+    const res: AxiosResponse<MenuInfo> = await instance.patch(
+      `/menus/${menuId}/menuname`,
+      updatemenuName
+    );
+
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    return 'edit menu name fail';
+  }
+};
+
+// 메뉴 삭제
+export const deleteMenu = async (menuId: number) => {
+  try {
+    const res: AxiosResponse<MenuInfo> = await instance.delete(`/menus/${menuId}`);
+
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    return 'delete menu fail';
+  }
+};
