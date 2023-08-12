3b1ad5176b2e119c46d4f1aadd2afd7f72bd381f
CHCHAENG <oco6029@naver.com>
2023-08-13 01:56:47 +0900

    Feat : menuList get, menu create 연결 완료

--- src/components/Project/Menu/MenuSlider.tsx
+++ src/components/Project/Menu/MenuSlider.tsx
@@ -1,7 +1,6 @@
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Slider from 'react-slick';
-import { MenuInfo } from '@/typings/menu.ts';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { IoIosArrowBack, IoIosArrowForward, IoIosClose } from 'react-icons/io';
 import styled from '@emotion/styled';
@@ -9,16 +8,17 @@ import { AiOutlinePlus } from 'react-icons/ai';
 import { Editable, EditableInput, EditablePreview, useDisclosure } from '@chakra-ui/react';
 import { useRecoilState } from 'recoil';
 import { menuListData } from '@/recoil/Project/Menu.ts';
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useMessage } from '@/hooks/useMessage.ts';
 import DeleteMenuDialog from '@/components/Project/Menu/DeleteMenuDialog.tsx';
 import { useMutation, useQuery, useQueryClient } from 'react-query';
 import { createMenu, editMenu, projectMenuList } from '@/api/Project/Menu.ts';
+import { MenuInfo } from '@/typings/menu.ts';
 
 interface Props {
   product: string;
   project: string;
-  menuTitle: string;
+  menutitle: string;
 }
 
 const StyledSlider = styled(Slider)`
@@ -39,12 +39,11 @@ const CustomNextSlider = styled.div`
   z-index: 3;
 `;
 
-export default function MenuSlider({ product, project, menuTitle }: Props) {
+export default function MenuSlider({ product, project, menutitle }: Props) {
   const { showMessage, contextHolder } = useMessage();
   const [menuList, setMenuList] = useRecoilState(menuListData);
   const [plusMenu, setPlusMenu] = useState(false);
   const navigate = useNavigate();
-  const [createMenuName, setCreateMenuName] = useState('');
   const [deleteMenuName, setDeleteMenuName] = useState('menuName');
   const [editMenuName, setEditMenuName] = useState('menuName');
   const [menuId, setMenuId] = useState(0);
@@ -52,66 +51,60 @@ export default function MenuSlider({ product, project, menuTitle }: Props) {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   // TODO : 현재 project id 값으로 바꾸기
-  const projectId = 1;
+  const projectId = 9;
+  const queryClient = useQueryClient();
 
   // menuList get
-  const getMenuList = useQuery(['menuList', project], () => projectMenuList(projectId), {
+  const getMenuList = useQuery(['menuList', projectId], () => projectMenuList(projectId), {
     staleTime: 60000, // 10분
     cacheTime: 80000,
     refetchOnMount: false, // 마운트(리렌더링)될 때 데이터를 다시 가져오지 않음
     refetchOnWindowFocus: false, // 브라우저를 포커싱했을때 데이터를 가져오지 않음
     refetchOnReconnect: false, // 네트워크가 다시 연결되었을때 다시 가져오지 않음
+    enabled: true,
   });
-
-  if (getMenuList.isSuccess) {
-    if (typeof getMenuList.data !== 'string' && 'id' in getMenuList.data) {
-      const menuList: MenuInfo[] = getMenuList.data;
-      setMenuList(menuList);
-    }
-  }
-
-  const queryClient = useQueryClient();
 
   // menu create
-  const { mutate: createMenuMutate } = useMutation(() => createMenu(projectId, createMenuName), {
-    onMutate: async () => {
-      console.log('요청', menuList);
-      // optimistic update를 덮어쓰지 않기 위해 쿼리를 수동으로 삭제
-      await queryClient.cancelQueries(['menuList']);
-
-      // 이전 값 저장
-      const previousData = queryClient.getQueryData(['menuList']);
-
-      // 새로운 값으로 optimistic ui 적용
-      queryClient.setQueryData(['menuList'], {
-        id: Math.max(...menuList.map((menu) => menu.id)) + 1,
-        menuName: createMenuName,
-        projectId: projectId,
-        version: '1.0.0',
-      });
-
-      // 에러가 난다면 원래것으로 설정
-      return () => queryClient.setQueryData(['menuList'], previousData);
-    },
-    onSuccess: (data) => {
-      if (typeof data !== 'string' && 'menuName' in data) {
-        showMessage('success', '메뉴가 생성되었습니다.');
-      }
-    },
-    onError: (error, value, rollback) => {
-      // rollback은 onMutate의 return값
-      if (rollback) {
-        rollback();
-        showMessage('error', '메뉴 생성에 실패했습니다.');
-      } else {
-        showMessage('error', '메뉴 생성에 실패했습니다.');
-      }
-    },
-    onSettled: () => {
-      // success or error, invalidate해서 새로 받아옴
-      return queryClient.invalidateQueries(['menuList']);
-    },
-  });
+  const { mutate: createMenuMutate } = useMutation(
+    (nextValue: string) => createMenu(projectId, nextValue),
+    {
+      // onMutate: async () => {
+      //   console.log('요청', menuList);
+      //   // optimistic update를 덮어쓰지 않기 위해 쿼리를 수동으로 삭제
+      //   await queryClient.cancelQueries(['menuList', projectId]);
+      //
+      //   // 이전 값 저장
+      //   const previousData: MenuInfo[] | undefined = queryClient.getQueryData([
+      //     'menuList',
+      //     projectId,
+      //   ]);
+      //   const addData = menuList[menuList.length - 1];
+      //   // 새로운 값으로 optimistic ui 적용
+      //   queryClient.setQueryData(['menuList', projectId], { menuList });
+      //
+      //   // 에러가 난다면 원래것으로 설정
+      //   return () => queryClient.setQueryData(['menuList', projectId], previousData);
+      // },
+      onSuccess: (data) => {
+        if (typeof data === 'object' && 'menuName' in data) {
+          showMessage('success', '메뉴가 생성되었습니다.');
+        }
+      },
+      // onError: (error, value, rollback) => {
+      //   // rollback은 onMutate의 return값
+      //   if (rollback) {
+      //     rollback();
+      //     showMessage('error', '메뉴 생성에 실패했습니다.');
+      //   } else {
+      //     showMessage('error', '메뉴 생성에 실패했습니다.');
+      //   }
+      // },
+      onSettled: () => {
+        // success or error, invalidate해서 새로 받아옴
+        return queryClient.invalidateQueries(['menuList', projectId]);
+      },
+    }
+  );
 
   // menu edit
   const { mutate: editMenuMutate } = useMutation(
@@ -119,18 +112,18 @@ export default function MenuSlider({ product, project, menuTitle }: Props) {
     {
       onMutate: async () => {
         // optimistic update를 덮어쓰지 않기 위해 쿼리를 수동으로 삭제
-        await queryClient.cancelQueries(['menuList']);
+        await queryClient.cancelQueries(['menuList', projectId]);
 
         // 이전 값 저장
-        const previousData = queryClient.getQueryData(['menuList']);
+        const previousData = queryClient.getQueryData(['menuList', projectId]);
 
         // 새로운 값으로 optimistic ui 적용
         queryClient.setQueryData(
-          ['menuList'],
+          ['menuList', projectId],
           menuList.map((menu) => (menu.id === menuId ? { ...menu, menuName: editMenuName } : menu))
         );
         // 에러가 난다면 원래것으로 설정
-        return () => queryClient.setQueryData(['menuList'], previousData);
+        return () => queryClient.setQueryData(['menuList', projectId], previousData);
       },
       onSuccess: (data) => {
         if (typeof data !== 'string' && 'menuName' in data) {
@@ -211,24 +204,33 @@ export default function MenuSlider({ product, project, menuTitle }: Props) {
 
       // 값을 입력했으면 새로운 값으로 메뉴 list에 추가
       if (editMenuName !== '') {
-        const newMenu: MenuInfo = {
-          id: Math.max(...menuList.map((menu) => menu.id)) + 1,
-          menuName: nextValue,
-          projectId: 1,
-          version: '1.0.0',
-        };
-        const updatedMenuList = [...menuList, newMenu];
-
-        setCreateMenuName(nextValue);
-        setMenuList(updatedMenuList);
+        // const newMenu: MenuInfo = {
+        //   id: Math.max(...menuList.map((menu) => menu.id)) + 1,
+        //   menuName: nextValue,
+        //   projectId: 1,
+        //   version: '1.0.0',
+        // };
+        // const updatedMenuList = [...menuList, newMenu];
+        // console.log(nextValue);
+        // setMenuList(updatedMenuList);
         setPlusMenu(false);
 
         // create post api 요청
-        createMenuMutate();
+        createMenuMutate(nextValue);
       }
     },
     [menuList, editMenuName]
   );
+
+  useEffect(() => {
+    if (getMenuList.data !== undefined) {
+      if (typeof getMenuList.data !== 'string') {
+        console.log('성공함');
+        console.log(getMenuList.data, menuList);
+        setMenuList(getMenuList.data);
+      }
+    }
+  }, [getMenuList.data]);
 
   // slide settings 커스텀
   const settings = {
@@ -252,59 +254,73 @@ export default function MenuSlider({ product, project, menuTitle }: Props) {
   return (
     <StyledSlider {...settings} className={'w-h-full text-[1.25rem] font-bold text-gray-border'}>
       {/*결과물 menu (defult)*/}
-      <NavLink
-        to={`/workspace/${product}/${project}/menu/결과물`}
-        className={({ isActive }) =>
-          `flex-row-center justify-start  m-auto h-[5rem] w-1/5 border-r border-gray-border ${
-            isActive && 'bg-orange text-black'
-          }`
-        }
-      >
-        <span className={'flex-row-center h-full w-full'}>
-          {menuList[0].menuName}
-          {contextHolder}
-        </span>
-        <DeleteMenuDialog isOpen={isOpen} onClose={onClose} menu={deleteMenuName} menuId={menuId} />
-      </NavLink>
+      {/*{menuList.length !== 0 && (*/}
+      {/*  <NavLink*/}
+      {/*    to={`/workspace/${product}/${project}/menu/결과물`}*/}
+      {/*    className={({ isActive }) =>*/}
+      {/*      `flex-row-center justify-start  m-auto h-[5rem] w-1/5 border-r border-gray-border ${*/}
+      {/*        isActive && 'bg-orange text-black'*/}
+      {/*      }`*/}
+      {/*    }*/}
+      {/*  >*/}
+      {/*    <span className={'flex-row-center h-full w-full'}>*/}
+      {/*      {menuList[0].menuName}*/}
+      {/*      {contextHolder}*/}
+      {/*    </span>*/}
+      {/*    <DeleteMenuDialog*/}
+      {/*      isOpen={isOpen}*/}
+      {/*      onClose={onClose}*/}
+      {/*      menu={deleteMenuName}*/}
+      {/*      menuId={menuId}*/}
+      {/*    />*/}
+      {/*  </NavLink>*/}
+      {/*)}*/}
 
-      {/*menuList 데이터*/}
-      {menuList.slice(1).map((menu) => (
-        <NavLink
-          to={`/workspace/${product}/${project}/menu/${menu.menuName}`}
-          className={({ isActive }) =>
-            `flex items-center justify-start self-start m-auto h-[5rem] w-1/5 relative border-r border-gray-border ${
-              isActive && 'bg-orange text-black'
-            }`
-          }
-          key={menu.id}
-        >
-          {menuTitle === menu.menuName && (
-            <IoIosClose
-              className={
-                'absolute right-2 top-0.5 text-transparent text-[2rem] hover:text-gray-dark'
-              }
-              onClick={() => {
-                setDeleteMenuName(menu.menuName);
-                setMenuId(menu.id);
-                onOpen();
-              }}
+      {contextHolder}
+      {menuList.length !== 0 &&
+        menuList.map((menu) => (
+          <NavLink
+            to={`/workspace/${product}/${project}/menu/${menu.menuName}`}
+            className={({ isActive }) =>
+              `flex items-center justify-start self-start m-auto h-[5rem] w-1/5 relative border-r border-gray-border ${
+                isActive && 'bg-orange text-black'
+              }`
+            }
+            key={menu.id}
+          >
+            <DeleteMenuDialog
+              isOpen={isOpen}
+              onClose={onClose}
+              menu={deleteMenuName}
+              menuId={menuId}
             />
-          )}
-          <span className={'flex-row-center h-full w-full'}>
-            {/*클릭해서 값 변경*/}
-            <Editable
-              value={menu.menuName}
-              placeholder={menu.menuName}
-              onChange={onChangeMenuName(menu.id)}
-              onSubmit={onSubmitMenuName(menu.id)}
-            >
-              <EditablePreview />
-              <EditableInput />
-            </Editable>
-          </span>
-        </NavLink>
-      ))}
-      {menuList.length < 15 && (
+            {menutitle === menu.menuName && (
+              <IoIosClose
+                className={
+                  'absolute right-2 top-0.5 text-transparent text-[2rem] hover:text-gray-dark'
+                }
+                onClick={() => {
+                  setDeleteMenuName(menu.menuName);
+                  setMenuId(menu.id);
+                  onOpen();
+                }}
+              />
+            )}
+            <span className={'flex-row-center h-full w-full'}>
+              {/*클릭해서 값 변경*/}
+              <Editable
+                value={menu.menuName}
+                placeholder={menu.menuName}
+                onChange={onChangeMenuName(menu.id)}
+                onSubmit={onSubmitMenuName(menu.id)}
+              >
+                <EditablePreview />
+                <EditableInput />
+              </Editable>
+            </span>
+          </NavLink>
+        ))}
+      {menuList.length < 15 && menuList.length !== 0 && (
         <div className={'flex-row-center h-[5rem] w-1/5'}>
           {plusMenu ? (
             <div className={'flex-row-center h-full text-[1.25rem] font-semibold text-black'}>
