364da93a1b6411e275c9af3fbed9be5c124ccfe2
5hyun <qhslsl@gmail.com>
2023-08-18 01:50:08 +0900

    Fix: 타입 에러 수정

--- src/pages/Product/Members.tsx
+++ src/pages/Product/Members.tsx
@@ -206,6 +206,7 @@ export default function Members() {
         newName: null,
         memberEmailList: memberEmailList,
         powerType: isLeader ? 'LEADER' : 'DEFAULT',
+        image: null,
       },
       productId,
     });
