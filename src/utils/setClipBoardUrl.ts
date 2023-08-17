6b16bfd15bf86a249d31f85e58ed0619507ae566
5hyun <qhslsl@gmail.com>
2023-08-18 02:27:19 +0900

    Fix: 수정

--- src/utils/setClipBoardUrl.ts
+++ src/utils/setClipBoardUrl.ts
@@ -7,5 +7,5 @@ export async function setClipBoardUrl(task: TaskData, location: string): string 
 
   const baseURL = await (isDeployment ? DEPLOYMENT_FRONTEND_URL : DEV_FRONTEND_URL);
 
-  return `http://61.109.214.41:10001/workspace/3/27`;
+  return `http://61.109.214.41:10001/workspace/3/27/menu/개발/task/21`;
 }
