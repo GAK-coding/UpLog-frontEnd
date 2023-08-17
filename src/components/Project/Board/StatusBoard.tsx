44ff39859584c27a041903f44d4e185603f2f24a
5hyun <qhslsl@gmail.com>
2023-08-18 01:08:23 +0900

    Feat: 스크롤 바 라이브러리 적용

--- src/components/Project/Board/StatusBoard.tsx
+++ src/components/Project/Board/StatusBoard.tsx
@@ -7,6 +7,7 @@ import { Draggable, Droppable } from 'react-beautiful-dnd';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useMessage } from '@/hooks/useMessage.ts';
 import { setClipBoardUrl } from '@/utils/setClipBoardUrl.ts';
+import { Scrollbars } from 'rc-scrollbars';
 
 interface Props {
   status: TaskStatus;
@@ -42,123 +43,145 @@ export default function StatusBoard({ status, tasks }: Props) {
           ref={provided.innerRef}
           {...provided.droppableProps}
         >
-          {contextHolder}
-          {/*제목, 개수*/}
-          <div
-            className={
-              'flex-row-center justify-between h-[3.5rem] px-[2.3rem] text-gray-dark text-[0.93rem]'
-            }
+          <Scrollbars
+            style={{ width: '100%', height: '100%' }}
+            autoHide
+            autoHideTimeout={1000}
+            // Duration for hide animation in ms.
+            autoHideDuration={200}
           >
-            <span className={'font-bold'}>{convertStatus}</span>
-            {tasks && <span>{`${tasks.length}개`}</span>}
-          </div>
+            {contextHolder}
+            {/*제목, 개수*/}
+            <div
+              className={
+                'flex-row-center justify-between h-[3.5rem] px-[2.3rem] text-gray-dark text-[0.93rem]'
+              }
+            >
+              <span className={'font-bold'}>{convertStatus}</span>
+              {tasks && <span>{`${tasks.length}개`}</span>}
+            </div>
 
-          {/*태스크들*/}
-          <div className={'flex-col items-center max-h-[85%] overflow-y-auto'}>
-            {tasks &&
-              tasks.map((task, index) => (
-                <Draggable draggableId={task.id.toString()} index={index} key={task.id.toString()}>
-                  {(provided, snapshot) => (
-                    <section
-                      ref={provided.innerRef}
-                      {...provided.draggableProps}
-                      {...provided.dragHandleProps}
-                      key={task.id}
-                      className={`flex-col min-w-[19.5rem] w-[85%] h-[8rem] bg-white rounded-[10px] mx-auto my-[0.5rem] px-[1.12rem] py-[0.5rem]
-                      ${snapshot.isDragging ? 'shadow-2xl shadow-gray-400' : ''}`}
-                      // onClick={(e) => {
-                      //   e.preventDefault();
-                      //   navigate(
-                      //     `/workspace/${product}/${project}/menu/${task.menuName}/task/${task.id}`
-                      //   );
-                      // }}
+            {/*태스크들*/}
+            <div className={'flex-col items-center h-[85%] overflow-y-auto'}>
+              <Scrollbars
+                style={{ height: '100%' }}
+                autoHide
+                autoHideTimeout={1000}
+                // Duration for hide animation in ms.
+                autoHideDuration={200}
+              >
+                {tasks &&
+                  tasks.map((task, index) => (
+                    <Draggable
+                      draggableId={task.id.toString()}
+                      index={index}
+                      key={task.id.toString()}
                     >
-                      {/*케밥 버튼*/}
-                      <div className={'flex justify-end h-[0.7rem] relative'}>
-                        <GoKebabHorizontal
-                          className={'fill-gray-dark cursor-pointer z-50'}
-                          onClick={(e) => {
-                            e.preventDefault();
-                            setIsClickTaskDetail((prevState) => ({
-                              ...prevState,
-                              [task.id]: !prevState[task.id],
-                            }));
-                          }}
-                        />
+                      {(provided, snapshot) => (
+                        <section
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          key={task.id}
+                          className={`flex-col min-w-[19.5rem] w-[85%] h-[8rem] bg-white rounded-[10px] mx-auto my-[0.5rem] px-[1.12rem] py-[0.5rem]
+                      ${snapshot.isDragging ? 'shadow-2xl shadow-gray-400' : ''}`}
+                          // onClick={(e) => {
+                          //   e.preventDefault();
+                          //   navigate(
+                          //     `/workspace/${product}/${project}/menu/${task.menuName}/task/${task.id}`
+                          //   );
+                          // }}
+                        >
+                          {/*케밥 버튼*/}
+                          <div className={'flex justify-end h-[0.7rem] relative'}>
+                            <GoKebabHorizontal
+                              className={'fill-gray-dark cursor-pointer z-50'}
+                              onClick={(e) => {
+                                e.preventDefault();
+                                setIsClickTaskDetail((prevState) => ({
+                                  ...prevState,
+                                  [task.id]: !prevState[task.id],
+                                }));
+                              }}
+                            />
 
-                        {
-                          /*케밥 버튼 클릭시*/
-                          isClickTaskDetail[task.id] && (
-                            <section
+                            {
+                              /*케밥 버튼 클릭시*/
+                              isClickTaskDetail[task.id] && (
+                                <section
+                                  className={
+                                    'absolute flex-col-center w-[5rem] h-[4.5rem] top-[1rem] task-detail-border cursor-pointer text-[0.5rem] text-gray-dark'
+                                  }
+                                >
+                                  <div
+                                    className={
+                                      'flex-row-center w-full h-1/2 hover:bg-orange-light-sideBar'
+                                    }
+                                    onClick={() => handleCopyClipBoard(task)}
+                                  >
+                                    링크복사
+                                  </div>
+                                  <div
+                                    className={
+                                      'flex-row-center w-full h-1/2 hover:bg-orange-light-sideBar'
+                                    }
+                                    onClick={() => {
+                                      navigate(
+                                        `${location.pathname}/menu/${task.menuName}/task/${task.id}`
+                                      );
+                                    }}
+                                  >
+                                    상세정보
+                                  </div>
+                                </section>
+                              )
+                            }
+                          </div>
+                          {/*task제목*/}
+                          <div className={'flex justify-start h-[2.5rem]'}>
+                            <span className={'text-[1rem]'}>{task.taskName}</span>
+                          </div>
+                          {/*그룹정보*/}
+                          <div className={'flex justify-start items-center mb-2'}>
+                            <span className={'text-gray-dark text-[0.65rem]'}>&nbsp;</span>
+                            {/*<span className={'text-gray-dark text-[0.65rem]'}>{task.teamName}</span>*/}
+                          </div>
+                          {/*menu, 할당자 정보 */}
+                          <div
+                            className={
+                              'flex-row-center justify-between text-[0.65rem] text-gray-dark'
+                            }
+                          >
+                            <span
                               className={
-                                'absolute flex-col-center w-[5rem] h-[4.5rem] top-[1rem] task-detail-border cursor-pointer text-[0.5rem] text-gray-dark'
+                                'flex items-center px-2 h-[1.5rem] rounded-[0.31rem] bg-orange-light-sideBar '
                               }
                             >
-                              <div
-                                className={
-                                  'flex-row-center w-full h-1/2 hover:bg-orange-light-sideBar'
-                                }
-                                onClick={() => handleCopyClipBoard(task)}
-                              >
-                                링크복사
-                              </div>
-                              <div
-                                className={
-                                  'flex-row-center w-full h-1/2 hover:bg-orange-light-sideBar'
-                                }
-                                onClick={() => {
-                                  navigate(
-                                    `${location.pathname}/menu/${task.menuName}/task/${task.id}`
-                                  );
-                                }}
-                              >
-                                상세정보
-                              </div>
-                            </section>
-                          )
-                        }
-                      </div>
-                      {/*task제목*/}
-                      <div className={'flex justify-start h-[2.5rem]'}>
-                        <span className={'text-[1rem]'}>{task.taskName}</span>
-                      </div>
-                      {/*그룹정보*/}
-                      <div className={'flex justify-start items-center mb-2'}>
-                        <span className={'text-gray-dark text-[0.65rem]'}>&nbsp;</span>
-                        {/*<span className={'text-gray-dark text-[0.65rem]'}>{task.teamName}</span>*/}
-                      </div>
-                      {/*menu, 할당자 정보 */}
-                      <div
-                        className={'flex-row-center justify-between text-[0.65rem] text-gray-dark'}
-                      >
-                        <span
-                          className={
-                            'flex items-center px-2 h-[1.5rem] rounded-[0.31rem] bg-orange-light-sideBar '
-                          }
-                        >
-                          {task.menuName}
-                        </span>
-                        <div className={'flex-row-center justify-between items-center'}>
-                          <span className={'px-2 text-[0.65rem] text-gray-dark'}>
-                            {task.targetMemberInfoDTO.name}
-                          </span>
-                          {!task.targetMemberInfoDTO.image ? (
-                            <FaUserCircle className={'flex text-[1.7rem] fill-gray-dark'} />
-                          ) : (
-                            <img
-                              src={task.targetMemberInfoDTO.image}
-                              alt="userprofile"
-                              className={'flex w-[1.7rem] h-[1.7rem]'}
-                            />
-                          )}
-                        </div>
-                      </div>
-                    </section>
-                  )}
-                </Draggable>
-              ))}
-            {provided.placeholder}
-          </div>
+                              {task.menuName}
+                            </span>
+                            <div className={'flex-row-center justify-between items-center'}>
+                              <span className={'px-2 text-[0.65rem] text-gray-dark'}>
+                                {task.targetMemberInfoDTO.name}
+                              </span>
+                              {!task.targetMemberInfoDTO.image ? (
+                                <FaUserCircle className={'flex text-[1.7rem] fill-gray-dark'} />
+                              ) : (
+                                <img
+                                  src={task.targetMemberInfoDTO.image}
+                                  alt="userprofile"
+                                  className={'flex w-[1.7rem] h-[1.7rem]'}
+                                />
+                              )}
+                            </div>
+                          </div>
+                        </section>
+                      )}
+                    </Draggable>
+                  ))}
+                {provided.placeholder}
+              </Scrollbars>
+            </div>
+          </Scrollbars>
         </section>
       )}
     </Droppable>
