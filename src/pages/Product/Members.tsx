b0ff946261d1c12426d456d8aa5bcd38716cad1b
5hyun <qhslsl@gmail.com>
2023-09-11 19:00:12 +0900

    Fix: 제품 멤버 가져오는 훅으로 변경

--- src/pages/Product/Members.tsx
+++ src/pages/Product/Members.tsx
@@ -12,20 +12,19 @@ import { Scrollbars } from 'rc-scrollbars';
 import AuthorityModal from '@/components/Product/Members/AuthorityModal.tsx';
 import MemberListManageAlert from '@/components/Product/Members/MemberListManageAlert.tsx';
 import { useMessage } from '@/hooks/useMessage.ts';
-import { useMutation, useQuery, useQueryClient } from 'react-query';
+import { useMutation, useQueryClient } from 'react-query';
 import {
   changeProductMemberRole,
-  getProductMemberList,
+  // getProductMemberList,
   productEdit,
 } from '@/api/Product/Product.ts';
 import { SaveUserInfo } from '@/typings/member.ts';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { productMemberList } from '@/recoil/Product/atom.ts';
-import { HiOutlineBuildingOffice } from 'react-icons/hi2';
 import { frontEndUrl } from '@/recoil/Common/atom.ts';
 import { FaUserCircle } from 'react-icons/fa';
 import { sendLog } from '@/api/Log';
-import show = Mocha.reporters.Base.cursor.show;
+import { useGetProductMembers } from '@/pages/Product/hooks/useGetProductMembers.ts';
 
 export default function Members() {
   const queryClient = useQueryClient();
@@ -95,43 +94,8 @@ export default function Members() {
     },
   });
 
-  const { data, isSuccess, refetch } = useQuery(
-    ['getProductMemberList', productId],
-    () => getProductMemberList(productId),
-    {
-      select: (data) => {
-        if (typeof data !== 'string') {
-          const temp: ProductMember[] = [];
-          data.forEach((member) => {
-            temp.push({
-              ...member,
-              isOpen: false,
-            });
-          });
-
-          const powerTypePriority = {
-            MASTER: 1,
-            LEADER: 2,
-            CLIENT: 3,
-            DEFAULT: 4,
-          };
-
-          const sortedData = temp.sort((a, b) => {
-            const priorityA = powerTypePriority[a.powerType];
-            const priorityB = powerTypePriority[b.powerType];
-            return priorityA - priorityB;
-          });
-
-          return sortedData;
-        }
-      },
-      staleTime: 60000, // 1분
-      cacheTime: 80000, // 1분 20초
-      refetchOnMount: false, // 마운트(리렌더링)될 때 데이터를 다시 가져오지 않음
-      refetchOnWindowFocus: false, // 브라우저를 포커싱했을때 데이터를 가져오지 않음
-      refetchOnReconnect: false, // 네트워크가 다시 연결되었을때 다시 가져오지 않음
-    }
-  );
+  // 제품 멤버 가져오는 훅
+  const [data, isSuccess, refetch] = useGetProductMembers(productId);
 
   const { mutate: changeProductMemberRoleMutate } = useMutation(changeProductMemberRole, {
     onSuccess: (data) => {
