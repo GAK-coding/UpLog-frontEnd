89578e8792b701dc7fee128ce6094c7d10ffb1c7
5hyun <qhslsl@gmail.com>
2023-09-21 00:12:03 +0900

    Chore: useQuery 옵션 추가

--- src/components/Product/hooks/useGetChildGroups.ts
+++ src/components/Product/hooks/useGetChildGroups.ts
@@ -8,6 +8,11 @@ export function useGetChildGroups(
   onSuccess?: (data: { childTeamInfoDTOList: ChildGroup[] } | 'fail getProjectTeams') => void
 ) {
   const { data, refetch } = useQuery(['childGroup', parentId], () => getChildGroups(parentId), {
+    staleTime: 300000, // 5분
+    cacheTime: 600000, // 10분
+    refetchOnMount: false, // 마운트(리렌더링)될 때 데이터를 다시 가져오지 않음
+    refetchOnWindowFocus: false, // 브라우저를 포커싱했을때 데이터를 가져오지 않음
+    refetchOnReconnect: false, // 네트워크가 다시 연결되었을때 다시 가져오지 않음
     enabled: enabled,
     onSuccess: (data) => onSuccess && onSuccess(data),
   });
