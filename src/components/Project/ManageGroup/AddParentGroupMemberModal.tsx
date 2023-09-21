fd724315890a9ef4f8e06083399f2d7216e825bc
5hyun <qhslsl@gmail.com>
2023-09-22 03:17:08 +0900

    Feat: 부모 팀에 멤버 추가하는 기능 구현

---
+++ src/components/Project/ManageGroup/AddParentGroupMemberModal.tsx
@@ -0,0 +1,134 @@
+import React, { useCallback, useState } from 'react';
+import { ProductMember } from '@/typings/product.ts';
+import { Scrollbars } from 'react-custom-scrollbars';
+import { addParentGroupMembers } from '@/api/Project/Version.ts';
+import { useMutation, useQueryClient } from 'react-query';
+import { ParentGroupMember } from '@/typings/project.ts';
+import { message } from '@/recoil/Common/atom.ts';
+import { useRecoilState } from 'recoil';
+
+interface Props {
+  childGroupAllMembers: number[];
+  productMembers: ProductMember[];
+  onCloseMemberAdd: () => void;
+}
+export default function AddParentGroupMemberModal({
+  childGroupAllMembers,
+  productMembers,
+  onCloseMemberAdd,
+}: Props) {
+  const nowParentGroupId = +sessionStorage.getItem('nowGroupId')!;
+  const [messageInfo, setMessageInfo] = useRecoilState(message);
+  const [selectMember, setSelectMember] = useState<Map<number, ParentGroupMember>>(new Map());
+
+  const onClickMember = useCallback(
+    (info: ProductMember) => {
+      const { memberId, memberEmail, memberName, memberNickName, powerType } = info;
+
+      if (Array.from(selectMember.keys()).includes(memberId)) {
+        setSelectMember((prev) => {
+          prev.delete(memberId);
+          return new Map(prev);
+        });
+      } else {
+        setSelectMember((prev) =>
+          new Map(prev).set(memberId, {
+            memberId,
+            memberEmail,
+            memberName,
+            memberNickname: memberNickName,
+            powerType,
+          })
+        );
+      }
+    },
+    [selectMember]
+  );
+
+  const queryClient = useQueryClient();
+
+  const { mutate } = useMutation(addParentGroupMembers, {
+    onMutate: async () => {
+      await queryClient.cancelQueries(['childGroupAllMembers', nowParentGroupId]);
+
+      const snapshot: ParentGroupMember[] | undefined = queryClient.getQueryData([
+        'childGroupAllMembers',
+        nowParentGroupId,
+      ]);
+
+      onCloseMemberAdd();
+      setMessageInfo({ type: 'success', content: '멤버가 추가되었습니다!' });
+
+      queryClient.setQueryData(['childGroupAllMembers', nowParentGroupId], () => {
+        return snapshot ? [...snapshot, ...selectMember.values()] : [...selectMember.values()];
+      });
+
+      return { snapshot };
+    },
+    onError: (err, newTodo, context) => {
+      queryClient.setQueryData(['childGroupAllMembers', nowParentGroupId], context?.snapshot);
+      setMessageInfo({ type: 'error', content: '멤버 추가를 실패하였습니다.' });
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries(['childGroupAllMembers', nowParentGroupId]);
+    },
+  });
+
+  const onClickAddMember = useCallback(() => {
+    mutate({
+      teamId: nowParentGroupId,
+      addMemberIdList: Array.from(selectMember.keys()),
+      link: '',
+    });
+  }, [nowParentGroupId, selectMember]);
+
+  return (
+    <div
+      className={
+        'absolute top-[3rem] right-0 w-48 h-80 z-20 bg-white shadow-sign-up rounded-t-[0.3rem]'
+      }
+      onClick={(e) => e.stopPropagation()}
+    >
+      <Scrollbars
+        style={{ width: '100%', height: '100%' }}
+        autoHide
+        autoHideTimeout={1000}
+        // Duration for hide animation in ms.
+        autoHideDuration={200}
+      >
+        {(productMembers as ProductMember[])?.map((member) => {
+          if (childGroupAllMembers?.includes(member.memberId) || !childGroupAllMembers) return;
+
+          const isSelect = Array.from(selectMember.keys()).includes(member.memberId);
+
+          return (
+            <div
+              key={member.memberId}
+              className={`flex p-2 cursor-pointer ${isSelect && 'bg-orange-light'}`}
+              onClick={() => onClickMember(member)}
+            >
+              <img
+                src={member.image ? member.image : '/images/test_userprofile.png'}
+                alt={`${member.memberNickName}의 프로필 서진`}
+                className={'w-10 h-10 mr-4 rounded-[50%]'}
+              />
+              <div className={'flex flex-col justify-center text-ms font-bold text-gray-dark'}>
+                <span className={'ellipsis'}>
+                  {member.memberNickName} ({member.memberName})
+                </span>
+              </div>
+            </div>
+          );
+        })}
+      </Scrollbars>
+      <div
+        className={
+          'flex-row-center w-full h-12 text-white font-bold bg-orange rounded-b-[0.3rem] cursor-pointer'
+        }
+        onClick={onClickAddMember}
+      >
+        추가하기
+      </div>
+    </div>
+  );
+}
