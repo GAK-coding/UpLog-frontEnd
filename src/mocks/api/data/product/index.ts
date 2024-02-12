d11df6f2a70332c8d200ef20ce93a18ec1d6799b
5hyun <qhslsl@gmail.com>
2024-02-12 21:51:54 +0900

    Chore: 제품 멤버 name, nickname 통일

--- src/mocks/api/data/product/index.ts
+++ src/mocks/api/data/product/index.ts
@@ -3,14 +3,16 @@ import { faker } from '@faker-js/faker';
 import { SaveUserInfo } from '@/typings/member.ts';
 
 const powerTypes = ['MASTER', 'LEADER', 'DEFAULT', 'CLIENT'];
+const name = faker.person.firstName();
+const nickname = faker.person.middleName();
 
 export function createProduct(): GetProductList {
   return {
     productId: faker.number.int(),
     productImage: faker.number.int({ min: 0, max: 1 }) % 2 === 0 ? null : faker.image.url(),
     productName: faker.company.name().slice(0, 6),
-    memberName: '',
-    memberNickname: '',
+    memberName: name,
+    memberNickname: nickname,
     powerType: powerTypes[faker.number.int({ min: 0, max: 3 })] as powerTypeList,
     indexNum: 0,
     delStatus: false,
